37018be9e11c67ffaef0439892f9da3d2bc40a4c
Hairum Qureshi <hqureshi@udel.edu>
2026-01-19 19:04:41 -0500

    created page routes for viewing role-play ads and creating a new ad

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
+import Advertisements from "../pages/Advertisements";
+import AdForm from "../pages/AdForm";
 
 export default function App() {
 	return (
@@ -11,6 +13,8 @@ export default function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/About" element={<About />} />
+				<Route path="/role-play-ads" element={<Advertisements />} />
+				<Route path="/new-ad" element={<AdForm />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
