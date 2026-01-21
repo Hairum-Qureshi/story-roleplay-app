2ab86c921c105ffa2ea2d6127adf1721c6eab091
Hairum Qureshi <hqureshi@udel.edu>
2026-01-21 00:43:19 -0500

    created navbar

---
+++ apps/frontend/src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import { FaUser } from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa6";
+import { FaPlusSquare } from "react-icons/fa";
+import { HiQuestionMarkCircle } from "react-icons/hi";
+
+export default function Navbar() {
+	return (
+		<div className="w-full h-16 bg-slate-900 border-b border-slate-700 flex items-center justify-center px-6 space-x-4 ">
+			<Link to="/">
+				<h1
+					className="border border-yellow-700 px-3 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-xl"
+					title="Main Feed"
+				>
+					<FaStar className="text-yellow-400" />
+				</h1>
+			</Link>
+			<Link to="/profile">
+				<h1
+					className="border border-slate-700 px-3 py-1 rounded-full h-10 w-10 text-xl font-bold text-zinc-100 flex items-center"
+					title="Profile"
+				>
+					<FaUser className="text-sky-400" />
+				</h1>
+			</Link>
+			<Link to="/inbox">
+				<h1
+					className="border border-slate-700 px-3 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-xl"
+					title="Messages"
+				>
+					<FaMessage className="text-sky-400" />
+				</h1>
+			</Link>
+			<Link to="/new-ad">
+				<h1
+					className="border border-slate-700 px-3 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-xl"
+					title="Create New Ad"
+				>
+					<FaPlusSquare className="text-sky-400" />
+				</h1>
+			</Link>
+			<Link to="/about">
+				<h1
+					className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
+					title="About"
+				>
+					<HiQuestionMarkCircle className="text-sky-400" />
+				</h1>
+			</Link>
+		</div>
+	);
+}
