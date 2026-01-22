b69b6690f76c98dca3c9de32e7717b5b351857cf
Hairum Qureshi <hqureshi@udel.edu>
2026-01-22 02:15:04 -0500

    configured CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
   console.log('Nest.js Server successfully started');
 }
 bootstrap();
