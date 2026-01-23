5937865e8c0ca00694de3625226566dd1d903828
Hairum Qureshi <hqureshi@udel.edu>
2026-01-23 18:07:47 -0500

    moved enableCors logic before the app.listen so that CORS runs correctly

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,12 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   });
+
+  await app.listen(process.env.PORT ?? 3000);
 
   console.log('Nest.js Server successfully started');
 }
