f61f93006b2f4131ef96be150fc840bfc0fe3c88
Hairum Qureshi <hqureshi@udel.edu>
2026-01-23 17:29:19 -0500

    updated one of the FAQ explanations

--- apps/frontend/src/pages/FAQ.tsx
+++ apps/frontend/src/pages/FAQ.tsx
@@ -56,7 +56,8 @@ export default function FAQ() {
 							role-play session. This will save the conversation history and is
 							a good way to wrap up your role-playing experience if you're done
 							your storyline or if you've lost interested and don't have plans
-							to continue.
+							to continue. Note that this will end the session for both you and
+							your role-play partner.
 						</p>
 					</div>
 					<div>
@@ -64,9 +65,10 @@ export default function FAQ() {
 							Can I undo the 'End Role-Play' button?
 						</h2>
 						<p className="text-lg">
-							No, once you (or your role-play partner) clicks the 'End Role-Play' button, the action is
-							permanent and cannot be undone. You will need to re-initiate a new
-							role-play session if you wish to continue.
+							No, once you (or your role-play partner) clicks the 'End
+							Role-Play' button, the action is permanent and cannot be undone.
+							You will need to re-initiate a new role-play session if you wish
+							to continue.
 						</p>
 					</div>
 				</div>
