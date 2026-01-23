ef300084a68745373f1206364dfaa2449dd25dcf
Hairum Qureshi <hqureshi@udel.edu>
2026-01-22 20:57:51 -0500

    finalized style for chat bubble, provided props to component so component content is unique and dynamic and appears right or left depending on if you sent the message; also added place for timestamp

--- apps/frontend/src/components/inbox/ChatBubble.tsx
+++ apps/frontend/src/components/inbox/ChatBubble.tsx
@@ -1,3 +1,19 @@
-export default function ChatBubble() {
-	return <div>ChatBubble</div>;
+interface ChatBubbleProps {
+	you: boolean;
+	message: string;
+}
+
+export default function ChatBubble({ you, message }: ChatBubbleProps) {
+	return (
+		<div className={`m-2 max-w-lg ${you ? "ml-auto" : "mr-auto"}`}>
+			<div
+				className={`relative rounded-2xl ${you ? "rounded-tr-sm bg-blue-950" : "rounded-tl-sm bg-gray-800"} px-4 py-3 shadow-md border border-gray-700`}
+			>
+				<p className="text-sm text-gray-100 leading-relaxed">{message}</p>
+				<span className="block mt-1 text-xs text-gray-400 text-right">
+					12:45 PM
+				</span>
+			</div>
+		</div>
+	);
 }
