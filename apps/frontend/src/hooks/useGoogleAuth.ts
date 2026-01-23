a2e777e8862a3c8dc65ccb9412fb7caa52909491
Hairum Qureshi <hqureshi@udel.edu>
2026-01-22 22:13:02 -0500

    created custom useGoogleAuth hook for handling logic pertaining to Google auth

---
+++ apps/frontend/src/hooks/useGoogleAuth.ts
@@ -0,0 +1,36 @@
+import { signInWithPopup } from "@firebase/auth";
+import { auth, googleProvider } from "../config/firebase";
+import axios from "axios";
+
+interface UseGoogleAuthHook {
+	googleSignInMutation: () => Promise<void>;
+}
+
+export default function useGoogleAuth(): UseGoogleAuthHook {
+	const handleGoogleSignIn = async () => {
+		try {
+			const result = await signInWithPopup(auth, googleProvider);
+			const token = await result.user.getIdToken();
+			console.log(token);
+			return token;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const googleSignInMutation = async () => {
+		const token = await handleGoogleSignIn();
+		await axios.post(
+			`${import.meta.env.VITE_BACKEND_BASE_URL}/api/auth/google/sign-in`,
+			{},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`
+				},
+				withCredentials: true
+			}
+		);
+	};
+
+	return { googleSignInMutation };
+}
