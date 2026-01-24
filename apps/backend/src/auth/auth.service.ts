17793f41a65aaa4d2985e4c731666ea692111692
Hairum Qureshi <hqureshi@udel.edu>
2026-01-24 18:15:14 -0500

    added 'maxAge' property to cookie creation logic

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -21,6 +21,7 @@ export class AuthService {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
     });
   }
 
