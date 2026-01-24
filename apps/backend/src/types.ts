20303589938ecdbeefd11ed0682c2ffd07f0c7f3
Hairum Qureshi <hqureshi@udel.edu>
2026-01-23 22:20:09 -0500

    added supporting types for UserPayload type

--- apps/backend/src/types.ts
+++ apps/backend/src/types.ts
@@ -1,13 +1,79 @@
+import { Types } from 'mongoose';
+
+type CharacterBio = {
+  _id: string;
+  name: string;
+  gender: string;
+  age: number;
+  appearance: string;
+  traits: string[];
+  author: Types.ObjectId;
+  backstory?: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+enum PovType {
+  FirstPerson = 'First Person',
+  SecondPerson = 'Second Person',
+  ThirdPerson = 'Third Person',
+}
+
+type RolePlayAd = {
+  _id: string;
+  title: string;
+  pov: PovType;
+  adultRoleplay: boolean;
+  premise: string;
+  traitsAndSkills: string[];
+  writingExpectations: string[];
+  contentNotes: string;
+  author: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+type Conversation = {
+  _id: string;
+  participants: Types.ObjectId[];
+  roleplayAd: Types.ObjectId;
+  title: string;
+  messages: Types.ObjectId[];
+  chatEnded: boolean;
+  characterBios?: Types.ObjectId[];
+  createdAt: Date;
+};
+
+type Message = {
+  _id: Types.ObjectId | string;
+  sender: Types.ObjectId | string;
+  conversation: Types.ObjectId | string;
+  content: string;
+};
+
 type UserPayload = {
-  // ! NOTE: modify the UserPayload type accordingly for your project if needed!
   _id: string;
   firstName: string;
   lastName: string;
   email: string;
+  profilePicture?: string;
+  username?: string;
+  characterBios: CharacterBio[];
+  conversations: Conversation[];
+  rolePlayAds: RolePlayAd[];
+  createdAt: Date;
 };
 
 type AuthRequest = Request & {
   user?: UserPayload;
-}; // this will bind the user object to the Request body
+};
 
-export type { UserPayload, AuthRequest };
+export type {
+  UserPayload,
+  AuthRequest,
+  RolePlayAd,
+  CharacterBio,
+  Conversation,
+  Message,
+  PovType,
+};
