0408a25d631621fbb79b53a1f9b2e17a1ce088a1
Hairum Qureshi <hqureshi@udel.edu>
2026-01-24 18:23:42 -0500

    exported 'PovType' type

--- apps/frontend/src/interfaces.ts
+++ apps/frontend/src/interfaces.ts
@@ -9,4 +9,59 @@ interface UseGoogleAuthHook {
 	signOut: () => Promise<void>;
 }
 
-export type { FeatureColumnProps, UseGoogleAuthHook };
+interface CharacterBio {
+	_id: string;
+	name: string;
+	gender: string;
+	age: number;
+	appearance: string;
+	traits: string[];
+	author: string;
+	backstory?: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+type PovType = "First Person" | "Second Person" | "Third Person";
+
+interface RolePlayAd {
+	_id: string;
+	title: string;
+	pov: PovType;
+	adultRoleplay: boolean;
+	premise: string;
+	traitsAndSkills: string[];
+	writingExpectations: string[];
+	contentNotes: string;
+	author: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+interface Conversation {
+	_id: string;
+	participants: string[];
+	roleplayAd: string;
+	title: string;
+	messages: string[];
+	chatEnded: boolean;
+	characterBios?: string[];
+	createdAt: Date;
+}
+
+interface Message {
+	_id: string;
+	sender: string;
+	conversation: string;
+	content: string;
+}
+
+export type {
+	FeatureColumnProps,
+	UseGoogleAuthHook,
+	CharacterBio,
+	RolePlayAd,
+	Conversation,
+	Message,
+	PovType
+};
