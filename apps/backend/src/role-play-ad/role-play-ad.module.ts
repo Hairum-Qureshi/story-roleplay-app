b2a8f06e4c9d6fbd6054d951309e859ae55679d6
Hairum Qureshi <hqureshi@udel.edu>
2026-01-25 03:34:33 -0500

    added MongoDB User and Roleplay Ad schema to imports

--- apps/backend/src/role-play-ad/role-play-ad.module.ts
+++ apps/backend/src/role-play-ad/role-play-ad.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { RolePlayAdService } from './role-play-ad.service';
 import { RolePlayAdController } from './role-play-ad.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schemas/User';
+import { RolePlayAd, RolePlayAdSchema } from 'src/schemas/RolePlayAd';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+    MongooseModule.forFeature([
+      {
+        name: RolePlayAd.name,
+        schema: RolePlayAdSchema,
+      },
+    ]),
+  ],
   providers: [RolePlayAdService],
   controllers: [RolePlayAdController],
 })
