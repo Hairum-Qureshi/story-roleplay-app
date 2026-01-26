5f6eedd2ed6c28059426249223fdeb8f0b928f9a
Hairum Qureshi <hqureshi@udel.edu>
2026-01-25 20:46:54 -0500

    moved cookie deletion logic to to controller sign out endpoint

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import {
-  Body,
   Controller,
   Get,
   Post,
@@ -14,27 +13,35 @@ import { CurrentUser } from 'src/decorators/currentUser.decorator';
 import * as types from 'src/types';
 import express from 'express';
 import { BearerToken } from 'src/decorators/bearerToken.decorator';
+import type { Response, CookieOptions } from 'express';
 
 @Controller('api/auth')
 export class AuthController {
-  constructor(private readonly userService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Post('google-sign-up')
+  @Post('google/sign-in')
   @UsePipes(new ValidationPipe())
-  googleAuth(@BearerToken() token: string): Promise<{ jwtToken: string }> {
-    return this.userService.googleAuth(token);
+  googleAuth(
+    @BearerToken() token: string,
+    @Res({ passthrough: true }) res: express.Response,
+  ): Promise<{ jwtToken: string }> {
+    return this.authService.googleAuth(token, res);
   }
 
   @Post('sign-out')
-  signOut(@Res({ passthrough: true }) res: express.Response): {
+  signOut(@Res({ passthrough: true }) res: Response): {
     message: string;
   } {
-    return this.userService.signOut(res);
+    res.clearCookie(
+      'auth-session',
+      this.authService.getAuthCookieOptions() as CookieOptions,
+    );
+    return { message: 'success' };
   }
 
   @Get('current-user')
   @UseGuards(AuthGuard())
   getCurrentUser(@CurrentUser() user: types.UserPayload): types.UserPayload {
-    return this.userService.getCurrentUser(user);
+    return this.authService.getCurrentUser(user);
   }
 }
