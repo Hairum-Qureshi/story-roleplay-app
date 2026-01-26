965682a545a195a009be394906a4793d36dccf26
Hairum Qureshi <hqureshi@udel.edu>
2026-01-25 20:47:26 -0500

    moved cookie deletion code to delete account endpoint

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from 'src/decorators/currentUser.decorator';
+import type { UserPayload } from 'src/types';
+import type { Response } from 'express';
+import { UserService } from './user.service';
+import { AuthService } from 'src/auth/auth.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('api/user')
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Delete('delete-account')
+  @UseGuards(AuthGuard())
+  deleteAccount(
+    @CurrentUser() user: UserPayload,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    this.userService.deleteUserById(user._id);
+
+    res.clearCookie('auth-session', this.authService.getAuthCookieOptions());
+  }
+}
