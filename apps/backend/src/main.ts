d64c3367fdeee5ef583a95782205e147ca25d439
Hairum Qureshi <hqureshi@udel.edu>
2026-01-25 21:28:30 -0500

    configured main.ts to handle static images

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(cookieParser());
 
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'assets'), {
+    prefix: '/assets/',
   });
 
   await app.listen(process.env.PORT ?? 3000);
