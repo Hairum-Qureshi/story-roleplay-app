988b90d125f2599039c4c015a86972e787d76f71
Hairum Qureshi <hqureshi@udel.edu>
2026-01-25 20:47:49 -0500

    added AuthModule to imports

--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -3,12 +3,24 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/User';
+import { CharacterBio, CharacterBioSchema } from 'src/schemas/CharacterBio';
+import { RolePlayAd, RolePlayAdSchema } from 'src/schemas/RolePlayAd';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      {
+        name: CharacterBio.name,
+        schema: CharacterBioSchema,
+      },
+      {
+        name: RolePlayAd.name,
+        schema: RolePlayAdSchema,
+      },
+    ]),
   ],
   providers: [UserService],
   controllers: [UserController],
