cb508e7871c178c4f97f940e8a52569a631fdfa7
Hairum Qureshi <hqureshi@udel.edu>
2026-01-27 00:29:29 -0500

    created 'createConversation' functionality logic

---
+++ apps/backend/src/chat/chat.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Conversation } from 'src/schemas/inbox/Conversation';
+import { RolePlayAd } from 'src/schemas/RolePlayAd';
+import type {
+  UserPayload,
+  ConversationDocument,
+  RolePlayAd as RolePlayAdInterface,
+} from 'src/types';
+import { User, UserDocument } from 'src/schemas/User';
+
+@Injectable()
+export class ChatService {
+  constructor(
+    @InjectModel(Conversation.name)
+    private conversationModel: Model<ConversationDocument>,
+    @InjectModel(RolePlayAd.name)
+    private rolePlayAdModel: Model<RolePlayAdInterface>,
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+
+  async createConversation(
+    adID: string,
+    user: UserPayload,
+  ): Promise<{
+    conversation: ConversationDocument;
+    rolePlayAd: RolePlayAdInterface;
+  }> {
+    // first need to check whether the user has a conversation with this exact ad already
+    const existingConversation: ConversationDocument | null =
+      await this.conversationModel.findOne({
+        ad: adID,
+        participants: user._id,
+      });
+
+    // get the ad data:
+    const ad: RolePlayAdInterface | null =
+      await this.rolePlayAdModel.findById(adID);
+    if (!ad) {
+      throw new Error('Role Play Ad not found');
+    }
+
+    if (existingConversation) {
+      // if they do, return the existing conversation
+      return { conversation: existingConversation, rolePlayAd: ad };
+    }
+
+    // if not, create a new conversation
+
+    // create the conversation with the ad and the user as participants
+
+    const newConversation: ConversationDocument =
+      await this.conversationModel.create({
+        participants: [user._id, ad.author],
+        messages: [],
+      });
+
+    // update both users' conversation arrays
+    await this.userModel.findByIdAndUpdate(user._id, {
+      $addToSet: { conversations: newConversation._id },
+    });
+
+    await this.userModel.findByIdAndUpdate(ad.author, {
+      $addToSet: { conversations: newConversation._id },
+    });
+
+    return { conversation: newConversation, rolePlayAd: ad };
+  }
+}
