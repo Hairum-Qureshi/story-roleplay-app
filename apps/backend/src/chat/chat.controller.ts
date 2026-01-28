677f2c35d8116d9aff78278aa61b923c132d739d
Hairum Qureshi <hqureshi@udel.edu>
2026-01-27 23:15:36 -0500

    created controller POST endpoint for creating message

--- apps/backend/src/chat/chat.controller.ts
+++ apps/backend/src/chat/chat.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Param, Post, UseGuards, Get } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards, Get, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from 'src/decorators/currentUser.decorator';
 import type { UserPayload } from 'src/types';
 import { ChatService } from './chat.service';
+import { CreateMessage } from 'src/DTOs/CreateMessage.dto';
 
-@Controller('chat')
+@Controller('api/chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
+
+  @Post('/:chatID/send-message')
+  @UseGuards(AuthGuard())
+  createConversationMessage(
+    @Param('chatID') chatID: string,
+    @Body() messageDto: CreateMessage,
+    @CurrentUser() user: UserPayload,
+  ) {
+    return this.chatService.createConversationMessage(chatID, messageDto, user);
+  }
 
   @Post('create/:adID')
   @UseGuards(AuthGuard())
