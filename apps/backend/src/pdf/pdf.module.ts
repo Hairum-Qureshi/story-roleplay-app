c7f286bb2536fdf408f95d87ea1ce9c9cdbf01ff
Hairum Qureshi <hqureshi@udel.edu>
2026-01-29 02:39:31 -0500

    added 'AuthModule' to imports array

---
+++ apps/backend/src/pdf/pdf.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PdfService } from './pdf.service';
+import { PdfController } from './pdf.controller';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [AuthModule],
+  providers: [PdfService],
+  controllers: [PdfController]
+})
+export class PdfModule {}
