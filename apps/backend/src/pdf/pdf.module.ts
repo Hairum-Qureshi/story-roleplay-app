8af2b172e29b922c726908516a9bb88ad637cbc3
Hairum Qureshi <hqureshi@udel.edu>
2026-01-29 02:43:24 -0500

    added Mongoose models to imports array

--- apps/backend/src/pdf/pdf.module.ts
+++ apps/backend/src/pdf/pdf.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { PdfService } from './pdf.service';
 import { PdfController } from './pdf.controller';
 import { AuthModule } from '../auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Conversation } from 'src/schemas/inbox/Conversation';
+import { ConversationSchema } from 'src/schemas/inbox/Conversation';
+import { User } from 'src/schemas/User';
+import { UserSchema } from 'src/schemas/User';
+import { Message } from 'src/schemas/inbox/Message';
+import { MessageSchema } from 'src/schemas/inbox/Message';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([
+      { name: Conversation.name, schema: ConversationSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Message.name, schema: MessageSchema },
+    ]),
+  ],
   providers: [PdfService],
-  controllers: [PdfController]
+  controllers: [PdfController],
 })
 export class PdfModule {}
