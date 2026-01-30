add3e24ba057302fffd9fd7051f831e76aff369a
Hairum Qureshi <hqureshi@udel.edu>
2026-01-30 03:19:41 -0500

    added PATCH endpoints for editing and deleting a message

--- apps/backend/src/chat/chat.controller.ts
+++ apps/backend/src/chat/chat.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Param, Post, UseGuards, Get, Body } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  Post,
+  UseGuards,
+  Get,
+  Body,
+  Patch,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from 'src/decorators/currentUser.decorator';
 import type { UserPayload } from 'src/types';
 import { ChatService } from './chat.service';
 import { CreateMessage } from 'src/DTOs/CreateMessage.dto';
+import { IsChatMember } from 'src/guards/IsChatMember.guard';
+import { EditMessage } from 'src/DTOs/EditMessage.dto';
 
 @Controller('api/chat')
 export class ChatController {
@@ -28,7 +38,7 @@ export class ChatController {
     return this.chatService.createConversation(adID, user);
   }
 
-  @Get('/:chatID/all-messages')
+  @Get(':chatID/all-messages')
   @UseGuards(AuthGuard())
   getAllMessagesInConversation(
     @Param('chatID') chatID: string,
@@ -41,5 +51,30 @@ export class ChatController {
   @UseGuards(AuthGuard())
   getAllConversations(@CurrentUser() user: UserPayload) {
     return this.chatService.getAllConversations(user);
+  }
+
+  @Patch(':chatID/end-conversation')
+  @UseGuards(AuthGuard(), IsChatMember)
+  endConversation(@Param('chatID') chatID: string) {
+    return this.chatService.endConversation(chatID);
+  }
+
+  @Patch(':chatID/:messageID/edit-message')
+  @UseGuards(AuthGuard(), IsChatMember)
+  editMessage(
+    @Param('chatID') chatID: string,
+    @Param('messageID') messageID: string,
+    @Body() messageDto: EditMessage,
+  ) {
+    return this.chatService.editMessage(chatID, messageID, messageDto);
+  }
+
+  @Patch(':chatID/:messageID/delete-message')
+  @UseGuards(AuthGuard(), IsChatMember)
+  deleteMessage(
+    @Param('chatID') chatID: string,
+    @Param('messageID') messageID: string,
+  ) {
+    return this.chatService.deleteMessage(chatID, messageID);
   }
 }
