47c181a9c137cd8074aff785797f87f756e3b6b4
Hairum Qureshi <hqureshi@udel.edu>
2026-01-29 20:51:56 -0500

    removed Message and User models from imports array

--- apps/backend/src/pdf/pdf.module.ts
+++ apps/backend/src/pdf/pdf.module.ts
@@ -5,18 +5,12 @@ import { AuthModule } from '../auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Conversation } from 'src/schemas/inbox/Conversation';
 import { ConversationSchema } from 'src/schemas/inbox/Conversation';
-import { User } from 'src/schemas/User';
-import { UserSchema } from 'src/schemas/User';
-import { Message } from 'src/schemas/inbox/Message';
-import { MessageSchema } from 'src/schemas/inbox/Message';
 
 @Module({
   imports: [
     AuthModule,
     MongooseModule.forFeature([
       { name: Conversation.name, schema: ConversationSchema },
-      { name: User.name, schema: UserSchema },
-      { name: Message.name, schema: MessageSchema },
     ]),
   ],
   providers: [PdfService],
