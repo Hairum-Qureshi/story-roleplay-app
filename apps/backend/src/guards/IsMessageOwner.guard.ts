9b9cc9764773ace2bb3afc798dac4e9a180db2b2
Hairum Qureshi <hqureshi@udel.edu>
2026-01-30 03:26:43 -0500

    created custom guard to check whether you're the owner of the message before making any alterations to the message

---
+++ apps/backend/src/guards/IsMessageOwner.guard.ts
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MessageDocument } from 'src/types';
+import { Message } from 'src/schemas/inbox/Message';
+
+@Injectable()
+export class IsMessageOwner implements CanActivate {
+  constructor(
+    @InjectModel(Message.name)
+    private messageModel: Model<MessageDocument>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    const messageID = request.params.messageID;
+
+    const message = await this.messageModel.findById(messageID).exec();
+
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+
+    if (message.sender !== user._id) {
+      throw new ForbiddenException('You are not the owner of this message');
+    }
+
+    return true;
+  }
+}
