90b7009c8b01d7eea159d46b71d8b0000dc889a7
Hairum Qureshi <hqureshi@udel.edu>
2026-01-31 00:44:03 -0500

    added redirect logic to redirect user back to '/inbox' route after removing a chat from their list

--- apps/frontend/src/hooks/useRolePlayChat.ts
+++ apps/frontend/src/hooks/useRolePlayChat.ts
@@ -273,6 +273,7 @@ export default function useRolePlayChat(chatID?: string): UseRolePlayChatHook {
 		},
 		onSuccess: () => {
 			queryClient.invalidateQueries({ queryKey: ["your-chats"] });
+			navigate("/inbox");
 		}
 	});
 
