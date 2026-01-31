3490f75224afdc2cc21622335e22c9b4882038ac
Hairum Qureshi <hqureshi@udel.edu>
2026-01-31 17:59:10 -0500

    added invalidate query call to refetch chats to display latest message when it's been edited

--- apps/frontend/src/hooks/useRolePlayChat.ts
+++ apps/frontend/src/hooks/useRolePlayChat.ts
@@ -236,6 +236,7 @@ export default function useRolePlayChat(chatID?: string): UseRolePlayChatHook {
 			queryClient.invalidateQueries({
 				queryKey: ["chat-messages", chatID]
 			});
+			queryClient.invalidateQueries({ queryKey: ["your-chats"] });
 		}
 	});
 
