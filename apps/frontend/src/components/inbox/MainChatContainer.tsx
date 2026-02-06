c5bb3b0268292b1d6381a193f5c20bca25f8252e
Hairum Qureshi <hqureshi@udel.edu>
2026-02-05 23:45:17 -0500

    added conditional logic to only show the typing indicator for a specific chat when the user is in the correct chat their partner is typing in

--- apps/frontend/src/components/inbox/MainChatContainer.tsx
+++ apps/frontend/src/components/inbox/MainChatContainer.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, use } from "react";
 import ChatBubble from "./ChatBubble";
 import useRolePlayChat from "../../hooks/useRolePlayChat";
 import type { Message, Conversation } from "../../interfaces";
@@ -6,20 +6,30 @@ import { useCurrentUser } from "../../hooks/useCurrentUser";
 import useSocketStore from "../../store/useSocketStore";
 import { useParams } from "react-router-dom";
 import Ad from "../Ad";
+import useTyping from "../../hooks/useTyping";
 
 export default function MainChatContainer({
 	selectedChat,
 	noMessageOpened,
 	fullWidth,
-	fullWidthToggle
+	fullWidthToggle,
+	partner,
+	partnerUsername
 }: {
 	selectedChat: Conversation | null;
 	noMessageOpened: boolean;
 	fullWidth: boolean;
 	fullWidthToggle: (fullWidth: boolean) => void;
+	partner: string;
+	partnerUsername: string;
 }) {
 	const { chatID } = useParams();
-	const { endedConversationID } = useSocketStore();
+	const {
+		endedConversationID,
+		typing,
+		partnerUsername: typingPartnerUsername,
+		currentTypingChatID
+	} = useSocketStore();
 
 	const {
 		sendMessage,
@@ -32,6 +42,8 @@ export default function MainChatContainer({
 	const [message, setMessage] = useState("");
 	const bottomOfContainer = useRef<HTMLDivElement>(null);
 	const isFirstLoad = useRef(true);
+
+	const { typingIndicator } = useTyping();
 
 	useEffect(() => {
 		if (!bottomOfContainer.current) return;
@@ -46,6 +58,8 @@ export default function MainChatContainer({
 			bottomOfContainer.current?.scrollIntoView({ behavior: "smooth" });
 		}
 	}, [rolePlayChatMessages]);
+
+	console.log(currentTypingChatID, selectedChat?._id);
 
 	return (
 		<div
@@ -128,10 +142,17 @@ export default function MainChatContainer({
 										isDeleted={message.isDeleted}
 										isEdited={message.isEdited || false}
 										messageID={message._id}
+										isSystemMessage={message.sender?.username === "SYSTEM"}
 									/>
 								)
 							)}
+
 							<div ref={bottomOfContainer} />
+							{typing && currentTypingChatID === selectedChat?._id && (
+								<p className="text-slate-400 italic ml-4 mb-2">
+									@{typingPartnerUsername} is typing...
+								</p>
+							)}
 						</div>
 						<div className="flex items-center w-full bg-slate-800 shadow-md px-4 py-2">
 							{selectedChat?.chatEnded || endedConversationID ? (
@@ -157,7 +178,14 @@ export default function MainChatContainer({
 										className="flex-1 bg-transparent w-full resize-none outline-none text-white placeholder-slate-400 h-17 p-2 focus:ring-0 rounded-md"
 										placeholder="Type your message..."
 										value={message}
-										onChange={e => setMessage(e.target.value)}
+										onChange={e => {
+											setMessage(e.target.value);
+											typingIndicator(
+												partner,
+												partnerUsername,
+												selectedChat?._id || ""
+											);
+										}}
 									/>
 									{selectedChat && (
 										<button
