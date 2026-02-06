b12134648efbc8f09b9a893598f8bfe82428fca7
Hairum Qureshi <hqureshi@udel.edu>
2026-02-06 14:39:34 -0500

    removed 'src' from User schema and model import and replaced it with '..'

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/schemas/User';
+import { User, UserSchema } from '../schemas/User';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt-strategy';
