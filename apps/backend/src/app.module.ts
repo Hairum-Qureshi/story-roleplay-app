fb074ea3a593631dbfe152c68880a79802e82234
Hairum Qureshi <hqureshi@udel.edu>
2026-02-06 10:47:08 -0500

    removed Messages Module and only importing modules that are being used

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MessagesModule } from './messages/messages.module';
+import { EventsModule } from './events/events.module';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RolePlayAdModule } from './role-play-ad/role-play-ad.module';
+import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
+import { PdfModule } from './pdf/pdf.module';
 
 @Module({
   imports: [
-    MessagesModule,
+    EventsModule,
     AuthModule,
-    UserModule,
     FirebaseModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI!),
+    RolePlayAdModule,
+    UserModule,
+    ChatModule,
+    PdfModule,
   ],
   controllers: [AppController],
   providers: [AppService],
