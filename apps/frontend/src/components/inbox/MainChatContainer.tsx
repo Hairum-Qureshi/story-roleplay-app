994b714c21b81fcd18f5d137a1ec7f29e19b1f12
Hairum Qureshi <hqureshi@udel.edu>
2026-02-06 00:06:29 -0500

    broke component up so it contains two child components: ChatHeader and ChatFooter

--- apps/frontend/src/components/inbox/MainChatContainer.tsx
+++ apps/frontend/src/components/inbox/MainChatContainer.tsx
@@ -1,12 +1,14 @@
-import { useRef, useState, useEffect, use } from "react";
+import { useRef, useEffect } from "react";
 import ChatBubble from "./ChatBubble";
 import useRolePlayChat from "../../hooks/useRolePlayChat";
-import type { Message, Conversation } from "../../interfaces";
+import type { Message } from "../../interfaces";
 import { useCurrentUser } from "../../hooks/useCurrentUser";
 import useSocketStore from "../../store/useSocketStore";
 import { useParams } from "react-router-dom";
 import Ad from "../Ad";
-import useTyping from "../../hooks/useTyping";
+import ChatHeader from "./ChatHeader";
+import ChatFooter from "./ChatFooter";
+import type { MainChatContainer } from "../../interfaces";
 
 export default function MainChatContainer({
 	selectedChat,
@@ -15,14 +17,7 @@ export default function MainChatContainer({
 	fullWidthToggle,
 	partner,
 	partnerUsername
-}: {
-	selectedChat: Conversation | null;
-	noMessageOpened: boolean;
-	fullWidth: boolean;
-	fullWidthToggle: (fullWidth: boolean) => void;
-	partner: string;
-	partnerUsername: string;
-}) {
+}: MainChatContainer) {
 	const { chatID } = useParams();
 	const {
 		endedConversationID,
@@ -31,19 +26,12 @@ export default function MainChatContainer({
 		currentTypingChatID
 	} = useSocketStore();
 
-	const {
-		sendMessage,
-		rolePlayChatMessages,
-		deleteMessage,
-		endRolePlayConversation
-	} = useRolePlayChat(chatID || "");
+	const { rolePlayChatMessages, deleteMessage, endRolePlayConversation } =
+		useRolePlayChat(chatID || "");
 
 	const { data: currUser } = useCurrentUser();
-	const [message, setMessage] = useState("");
 	const bottomOfContainer = useRef<HTMLDivElement>(null);
 	const isFirstLoad = useRef(true);
-
-	const { typingIndicator } = useTyping();
 
 	useEffect(() => {
 		if (!bottomOfContainer.current) return;
@@ -71,47 +59,13 @@ export default function MainChatContainer({
 				</p>
 			) : (
 				<>
-					<div className="w-full border-b border-slate-700 flex items-center">
-						<h3
-							className={
-								fullWidth
-									? "text-lg w-[54%] truncate overflow-hidden font-semibold mr-auto ml-4"
-									: "w-[32%] ml-3 truncate font-semibold text-lg"
-							}
-						>
-							{selectedChat?.title}
-						</h3>
-						<div
-							className={`m-2 bg-red-500 border border-red-600 rounded-md px-2 py-1 ${
-								selectedChat?.chatEnded || endedConversationID
-									? "cursor-not-allowed opacity-50"
-									: "hover:bg-red-600 active:bg-red-700 hover:cursor-pointer"
-							}`}
-							onClick={() => {
-								if (!selectedChat?.chatEnded && !endedConversationID) {
-									endRolePlayConversation(selectedChat!._id);
-								}
-							}}
-						>
-							{selectedChat?.chatEnded || endedConversationID
-								? "Ended"
-								: "End Role-Play"}
-						</div>
-						<select className="m-2 border border-white rounded-md px-2 py-1 hover:cursor-pointer">
-							<option value="" disabled selected>
-								Choose Character
-							</option>
-							<option value="character1">Character 1</option>
-							<option value="character2">Character 2</option>
-							<option value="character3">Character 3</option>
-						</select>
-						<button
-							onClick={() => fullWidthToggle(!fullWidth)}
-							className="m-2 border border-white rounded-md px-2 py-1 hover:cursor-pointer justify-end"
-						>
-							{fullWidth ? "Show" : "Hide"} Side Panel
-						</button>
-					</div>
+					<ChatHeader
+						fullWidth={fullWidth}
+						fullWidthToggle={fullWidthToggle}
+						selectedChat={selectedChat}
+						endedConversationID={endedConversationID}
+						endRolePlayConversation={endRolePlayConversation}
+					/>
 					<div className="relative">
 						<div className="overflow-y-scroll h-[75vh]">
 							{selectedChat && (
@@ -173,32 +127,11 @@ export default function MainChatContainer({
 									</p>
 								</div>
 							) : (
-								<div className="flex items-center space-x-2 w-full">
-									<textarea
-										className="flex-1 bg-transparent w-full resize-none outline-none text-white placeholder-slate-400 h-17 p-2 focus:ring-0 rounded-md"
-										placeholder="Type your message..."
-										value={message}
-										onChange={e => {
-											setMessage(e.target.value);
-											typingIndicator(
-												partner,
-												partnerUsername,
-												selectedChat?._id || ""
-											);
-										}}
-									/>
-									{selectedChat && (
-										<button
-											className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 transition-colors duration-200 rounded-md"
-											onClick={() => {
-												sendMessage(selectedChat._id, message);
-												setMessage("");
-											}}
-										>
-											Send
-										</button>
-									)}
-								</div>
+								<ChatFooter
+									partner={partner}
+									partnerUsername={partnerUsername}
+									selectedChat={selectedChat}
+								/>
 							)}
 						</div>
 					</div>
