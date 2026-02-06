d7282a216c0bd2ad7d8efb5b055f12f50c7e93a0
Hairum Qureshi <hqureshi@udel.edu>
2026-02-06 14:40:02 -0500

    removed 'src' from User schema and model import and replaced it with '..'

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from 'src/schemas/User';
+import { User, UserDocument } from '../schemas/User';
 import * as admin from 'firebase-admin';
 import { JwtService } from '@nestjs/jwt';
 import { UserPayload } from 'src/types';
