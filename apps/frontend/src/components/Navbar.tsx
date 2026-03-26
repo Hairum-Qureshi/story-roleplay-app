00b993055bc079cfe3c0f95bd5228deb6ef04a85
Hairum Qureshi <hqureshi@udel.edu>
2026-03-25 22:58:07 -0400

    redid navbar layout

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,148 +1,151 @@
-import { FaUser } from "react-icons/fa";
-import { FaMessage } from "react-icons/fa6";
 import { Link, useLocation } from "react-router-dom";
-import { FaStar } from "react-icons/fa6";
-import { FaPlusSquare } from "react-icons/fa";
-import { HiQuestionMarkCircle } from "react-icons/hi";
 import { IoHomeSharp } from "react-icons/io5";
-import { FaUserPlus } from "react-icons/fa";
-import { FaUsers } from "react-icons/fa";
-import { IoIosInformationCircle } from "react-icons/io";
-import { RiLogoutBoxRFill } from "react-icons/ri";
 import { useCurrentUser } from "../hooks/useCurrentUser";
-import { FaHeart } from "react-icons/fa";
-import { MdTipsAndUpdates } from "react-icons/md";
-import { IoMail } from "react-icons/io5";
-import { FaGavel } from "react-icons/fa6";
 import useGoogleAuth from "../hooks/useGoogleAuth";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
 
 export default function Navbar() {
 	const location = useLocation();
 	const { data: currUserData } = useCurrentUser();
 	const { signOut } = useGoogleAuth();
+	const [showMenu, setMenuVisibility] = useState(false);
 
 	return (
 		<div
-			className={`w-full h-16 bg-slate-900 border-b border-slate-700 flex items-center justify-center px-6 space-x-4 ${location.pathname !== "/inbox" && location.pathname !== "/faq" && "sticky top-0 z-50"}`}
+			className={`w-full h-16 text-white bg-slate-900/95 backdrop-blur border-b border-slate-800 flex items-center px-6 ${
+				location.pathname !== "/inbox" && location.pathname !== "/faq"
+					? "sticky top-0 z-50"
+					: ""
+			}`}
 		>
-			<Link to="/">
-				<h1
-					className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 text-xl font-bold text-zinc-100 flex items-center justify-center"
-					title="Home"
-				>
-					<IoHomeSharp className="text-sky-400" />
-				</h1>
-			</Link>
-			<Link to="/role-play-ads">
-				<h1
-					className="border border-yellow-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center justify-center text-xl"
-					title="Main Feed"
-				>
-					<FaStar className="text-yellow-400" />
-				</h1>
-			</Link>
-			{currUserData && (
-				<>
-					<Link to="/profile">
-						<h1
-							className="border border-slate-700 px-3 py-1 rounded-full h-10 w-10 text-xl font-bold text-zinc-100 flex items-center"
-							title="Profile"
-						>
-							<FaUser className="text-sky-400" />
-						</h1>
-					</Link>
-					<Link to="/inbox">
-						<h1
-							className="border border-slate-700 px-3 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-xl"
-							title="Messages"
-						>
-							<FaMessage className="text-sky-400" />
-						</h1>
-					</Link>
-					<Link to="/new-ad">
-						<h1
-							className="border border-slate-700 px-3 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-xl"
-							title="Create New Ad"
-						>
-							<FaPlusSquare className="text-sky-400" />
-						</h1>
-					</Link>
-					<Link to="/new-character">
-						<h1
-							className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-							title="Create Character"
-						>
-							<FaUserPlus className="text-sky-400" />
-						</h1>
-					</Link>
-					<Link to="/character-bios">
-						<h1
-							className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-							title="Character Bios"
-						>
-							<FaUsers className="text-sky-400" />
-						</h1>
-					</Link>
-					<Link to="/favorited-ads">
-						<h1
-							className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-							title="Favorited Ads"
-						>
-							<FaHeart className="text-sky-400" />
-						</h1>
-					</Link>
-					<Link to="/contact">
-						<h1
-							className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-							title="Contact"
-						>
-							<IoMail className="text-sky-400" />
-						</h1>
-					</Link>
-				</>
-			)}
-			<Link to="/faq">
-				<h1
-					className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-					title="FAQ"
-				>
-					<IoIosInformationCircle className="text-sky-400" />
-				</h1>
-			</Link>
-			<Link to="/updates-changelog">
-				<h1
-					className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-					title="Updates and Changelog"
-				>
-					<MdTipsAndUpdates className="text-sky-400" />
-				</h1>
-			</Link>
-			<Link to="/about">
-				<h1
-					className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-					title="About"
-				>
-					<HiQuestionMarkCircle className="text-sky-400" />
-				</h1>
-			</Link>
-			<Link to="/guidelines">
-				<h1
-					className="border border-slate-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-					title="Guidelines"
-				>
-					<FaGavel className="text-sky-400" />
-				</h1>
-			</Link>
-			{currUserData && (
-				<Link to="/" onClick={() => signOut()}>
-					<h1
-						className="border border-red-700 px-2 py-1 rounded-full h-10 w-10 font-bold text-zinc-100 flex items-center text-2xl"
-						title="Logout"
-					>
-						<RiLogoutBoxRFill className="text-red-600" />
-					</h1>
+			{/* LEFT */}
+			<div className="flex items-center gap-6">
+				<Link to="/" className="flex items-center">
+					<IoHomeSharp className="text-sky-400 text-xl hover:scale-110 transition" />
 				</Link>
-			)}
+
+				<Link
+					to="/role-play-ads"
+					className="text-base font-medium text-zinc-300 hover:text-white transition"
+				>
+					Main Feed
+				</Link>
+
+				{currUserData && (
+					<>
+						<Link
+							to="/inbox"
+							className="text-base font-medium text-zinc-300 hover:text-white transition"
+						>
+							Inbox
+						</Link>
+						<Link
+							className="hover:cursor-pointer px-2 py-1 hover:bg-slate-800 hover:rounded-md"
+							to="/favorited-ads"
+						>
+							Favorited Ads
+						</Link>
+						<Link
+							className="hover:cursor-pointer px-2 py-1 hover:bg-slate-800 hover:rounded-md"
+							to="/new-ad"
+						>
+							Create Ad
+						</Link>
+						<Link
+							className="hover:cursor-pointer px-2 py-1 hover:bg-slate-800 hover:rounded-md"
+							to="/new-character"
+						>
+							Create Character
+						</Link>
+						<Link
+							className="hover:cursor-pointer px-2 py-1 hover:bg-slate-800 hover:rounded-md"
+							to="/character-bios"
+						>
+							Character Bios
+						</Link>
+					</>
+				)}
+			</div>
+
+			{/* RIGHT */}
+			<div className="ml-auto flex items-center gap-4">
+				{currUserData ? (
+					<div className="relative group">
+						<button
+							className="px-3 py-1.5 text-2xl hover:cursor-pointer text-zinc-200 rounded-md hover:bg-slate-700"
+							onClick={() => setMenuVisibility(prev => !prev)}
+						>
+							{showMenu ? <IoCloseSharp /> : <GiHamburgerMenu />}
+						</button>
+						{/* Dropdown */}
+						{showMenu && (
+							<div className="absolute right-0 mt-2 w-56 bg-slate-900 border border-slate-700 rounded-md shadow-lg">
+								<div className="flex flex-col text-sm p-3 space-y-2">
+									<div className="flex items-center transition hover:cursor-pointer px-2 py-1 hover:bg-slate-800 hover:rounded-md">
+										<img
+											src={currUserData.profilePicture}
+											alt="Profile"
+											className="w-7 h-7 rounded-full mr-3"
+										/>
+										<Link className="" to="/profile">
+											Profile
+										</Link>
+									</div>
+									<div className="border-t border-slate-700 my-1" />
+									<Link
+										className="hover:cursor-pointer transition px-2 py-1 mt-2 hover:bg-slate-800 hover:rounded-md"
+										to="/contact"
+									>
+										Contact
+									</Link>
+									<Link
+										className="hover:cursor-pointer transition px-2 py-1 hover:bg-slate-800 hover:rounded-md"
+										to="/faq"
+									>
+										FAQ
+									</Link>
+									<Link
+										className="hover:cursor-pointer transition  px-2 py-1 hover:bg-slate-800 hover:rounded-md"
+										to="/about"
+									>
+										About
+									</Link>
+									<Link
+										className="hover:cursor-pointer transition px-2 py-1 hover:bg-slate-800 hover:rounded-md"
+										to="/guidelines"
+									>
+										Guidelines
+									</Link>
+									<Link
+										className="hover:cursor-pointer transition px-2 py-1 hover:bg-slate-800 hover:rounded-md"
+										to="/updates-changelog"
+									>
+										Updates
+									</Link>
+
+									<div className="border-t border-slate-700 my-1" />
+
+									<button
+										onClick={signOut}
+										className="text-left px-4 py-2 hover:cursor-pointer rounded-md text-red-500 hover:bg-slate-800 transition"
+									>
+										Sign Out
+									</button>
+								</div>
+							</div>
+						)}
+					</div>
+				) : (
+					<Link
+						to="/login"
+						className="text-sm font-medium text-zinc-300 hover:text-white"
+					>
+						Login
+					</Link>
+				)}
+			</div>
 		</div>
 	);
 }
