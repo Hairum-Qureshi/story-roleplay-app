be9d61ee5899da1e82c94bd27681a977551fac43
Hairum Qureshi <hqureshi@udel.edu>
2026-07-05 23:23:02 -0400

    added mutation hook for pinning messages

--- apps/frontend/src/hooks/useRolePlayChat.ts
+++ apps/frontend/src/hooks/useRolePlayChat.ts
@@ -7,324 +7,360 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface UseRolePlayChatHook {
-	createConversation: (adID: string) => void;
-	rolePlayChats: Conversation[];
-	sendMessage: (adID: string, message: string) => void;
-	rolePlayChatMessages: Message[];
-	deleteMessage: (chatID: string, messageID: string) => void;
-	editMessage: (
-		chatID: string,
-		messageID: string,
-		editedMessage: string
-	) => void;
-	endRolePlayConversation: (chatID: string) => void;
-	removeChatFromList: (chatID: string) => void;
-	currUserConversations: { _id: string; conversations: Conversation[] };
+  createConversation: (adID: string) => void;
+  rolePlayChats: Conversation[];
+  sendMessage: (adID: string, message: string) => void;
+  rolePlayChatMessages: Message[];
+  deleteMessage: (chatID: string, messageID: string) => void;
+  editMessage: (
+    chatID: string,
+    messageID: string,
+    editedMessage: string,
+  ) => void;
+  endRolePlayConversation: (chatID: string) => void;
+  removeChatFromList: (chatID: string) => void;
+  currUserConversations: { _id: string; conversations: Conversation[] };
+  pinMessageMutation: ({
+    chatID,
+    messageID,
+  }: {
+    chatID: string;
+    messageID: string;
+  }) => void;
 }
 
 export default function useRolePlayChat(chatID?: string): UseRolePlayChatHook {
-	const queryClient = useQueryClient();
-	const { message } = useSocketStore();
-	const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const { message } = useSocketStore();
+  const navigate = useNavigate();
 
-	const { mutate } = useMutation<
-		AxiosResponse<{ conversation: Conversation }>,
-		unknown,
-		{ adID: string }
-	>({
-		mutationFn: async ({ adID }: { adID: string }) => {
-			try {
-				const response = await axios.post<{ conversation: Conversation }>(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/create/${adID}`,
-					{},
-					{
-						withCredentials: true
-					}
-				);
-				return response;
-			} catch (error) {
-				console.error(error);
-				throw error;
-			}
-		},
-		onSuccess: data => {
-			navigate(`/inbox/${data.data.conversation._id}`);
-		}
-	});
+  const { mutate } = useMutation<
+    AxiosResponse<{ conversation: Conversation }>,
+    unknown,
+    { adID: string }
+  >({
+    mutationFn: async ({ adID }: { adID: string }) => {
+      try {
+        const response = await axios.post<{ conversation: Conversation }>(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/create/${adID}`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+        return response;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    onSuccess: (data) => {
+      navigate(`/inbox/${data.data.conversation._id}`);
+    },
+  });
 
-	const { mutate: messageMutation } = useMutation({
-		mutationFn: async ({
-			chatID,
-			message
-		}: {
-			chatID: string;
-			message: string;
-		}) => {
-			try {
-				await axios.post(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/send-message`,
-					{ message },
-					{
-						withCredentials: true
-					}
-				);
-			} catch (error) {
-				console.error(error);
-			}
-		},
-		onSuccess: () => {
-			queryClient.invalidateQueries({
-				queryKey: ["chat-messages", chatID]
-			});
-		}
-	});
+  const { mutate: messageMutation } = useMutation({
+    mutationFn: async ({
+      chatID,
+      message,
+    }: {
+      chatID: string;
+      message: string;
+    }) => {
+      try {
+        await axios.post(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/send-message`,
+          { message },
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["chat-messages", chatID],
+      });
+    },
+  });
 
-	const { data: rolePlayChatMessages } = useQuery({
-		queryKey: ["chat-messages", chatID],
-		queryFn: async () => {
-			try {
-				if (!chatID) return [];
+  const { data: rolePlayChatMessages } = useQuery({
+    queryKey: ["chat-messages", chatID],
+    queryFn: async () => {
+      try {
+        if (!chatID) return [];
 
-				const response = await axios.get(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/all-messages`,
-					{
-						withCredentials: true
-					}
-				);
-				return response.data;
-			} catch (error) {
-				console.error(error);
-			}
-		}
-	});
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/all-messages`,
+          {
+            withCredentials: true,
+          },
+        );
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
 
-	const { data: rolePlayChats } = useQuery({
-		queryKey: ["roleplay-chats-data"],
-		queryFn: async () => {
-			try {
-				const response = await axios.get(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/all-data`,
-					{
-						withCredentials: true
-					}
-				);
-				return response.data;
-			} catch (error) {
-				console.error(error);
-			}
-		}
-	});
+  const { data: rolePlayChats } = useQuery({
+    queryKey: ["roleplay-chats-data"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/all-data`,
+          {
+            withCredentials: true,
+          },
+        );
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
 
-	const { data: currUserConversations } = useQuery({
-		queryKey: ["your-chats"],
-		queryFn: async () => {
-			try {
-				const response = await axios.get(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/all/user-chats`,
-					{
-						withCredentials: true
-					}
-				);
-				return response.data;
-			} catch (error) {
-				console.error(error);
-			}
-		}
-	});
+  const { data: currUserConversations } = useQuery({
+    queryKey: ["your-chats"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/all/user-chats`,
+          {
+            withCredentials: true,
+          },
+        );
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
 
-	function sendMessage(chatID: string, message: string) {
-		if (!message.trim()) return alert("Message cannot be empty");
+  function sendMessage(chatID: string, message: string) {
+    if (!message.trim()) return alert("Message cannot be empty");
 
-		if (message.length > 2000) {
-			return alert("Message cannot exceed 2000 characters");
-		}
+    if (message.length > 2000) {
+      return alert("Message cannot exceed 2000 characters");
+    }
 
-		messageMutation({ chatID, message });
-	}
+    messageMutation({ chatID, message });
+  }
 
-	function createConversation(adID: string) {
-		mutate({ adID });
-	}
+  function createConversation(adID: string) {
+    mutate({ adID });
+  }
 
-	useEffect(() => {
-		if (!message || message.conversation !== chatID) return;
+  useEffect(() => {
+    if (!message || message.conversation !== chatID) return;
 
-		queryClient.setQueryData<Message[]>(
-			["chat-messages", chatID],
-			(oldMessages = []) => {
-				if (oldMessages.some(m => m._id === message._id)) {
-					return oldMessages;
-				}
-				return [...oldMessages, message];
-			}
-		);
+    queryClient.setQueryData<Message[]>(
+      ["chat-messages", chatID],
+      (oldMessages = []) => {
+        if (oldMessages.some((m) => m._id === message._id)) {
+          return oldMessages;
+        }
+        return [...oldMessages, message];
+      },
+    );
 
-		// Browser API for creating a notification
-		if (document.visibilityState === "hidden") {
-			if (Notification.permission === "granted") {
-				new Notification(`New message from @${message.sender.username}`, {
-					body: message.content,
-					icon: message.sender.profilePicture
-				});
-			} else if (Notification.permission !== "denied") {
-				Notification.requestPermission().then(permission => {
-					if (permission === "granted") {
-						new Notification(`New message from @${message.sender.username}`, {
-							body: message.content,
-							icon: message.sender.profilePicture
-						});
-					}
-				});
-			}
-		}
-	}, [message, chatID]);
+    // Browser API for creating a notification
+    if (document.visibilityState === "hidden") {
+      if (Notification.permission === "granted") {
+        new Notification(`New message from @${message.sender.username}`, {
+          body: message.content,
+          icon: message.sender.profilePicture,
+        });
+      } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            new Notification(`New message from @${message.sender.username}`, {
+              body: message.content,
+              icon: message.sender.profilePicture,
+            });
+          }
+        });
+      }
+    }
+  }, [message, chatID]);
 
-	const { mutate: endChatMutation } = useMutation({
-		mutationFn: async ({ chatID }: { chatID: string }) => {
-			try {
-				await axios.patch(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/end-conversation`,
-					{},
-					{
-						withCredentials: true
-					}
-				);
-			} catch (error) {
-				console.error(error);
-			}
-		}
-	});
+  const { mutate: endChatMutation } = useMutation({
+    mutationFn: async ({ chatID }: { chatID: string }) => {
+      try {
+        await axios.patch(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/end-conversation`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
 
-	const { mutate: deleteMessageMutation } = useMutation({
-		mutationFn: async ({
-			chatID,
-			messageID
-		}: {
-			chatID: string;
-			messageID: string;
-		}) => {
-			try {
-				await axios.patch(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/${messageID}/delete-message`,
-					{},
-					{
-						withCredentials: true
-					}
-				);
-			} catch (error) {
-				console.error(error);
-			}
-		},
-		onSuccess: () => {
-			queryClient.invalidateQueries({
-				queryKey: ["chat-messages", chatID]
-			});
-		}
-	});
+  const { mutate: deleteMessageMutation } = useMutation({
+    mutationFn: async ({
+      chatID,
+      messageID,
+    }: {
+      chatID: string;
+      messageID: string;
+    }) => {
+      try {
+        await axios.patch(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/${messageID}/delete-message`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["chat-messages", chatID],
+      });
+    },
+  });
 
-	function deleteMessage(chatID: string, messageID: string) {
-		const confirmation = confirm(
-			"Are you sure you want to delete this message?"
-		);
-		if (!confirmation) return;
+  function deleteMessage(chatID: string, messageID: string) {
+    const confirmation = confirm(
+      "Are you sure you want to delete this message?",
+    );
+    if (!confirmation) return;
 
-		deleteMessageMutation({ chatID, messageID });
-	}
+    deleteMessageMutation({ chatID, messageID });
+  }
 
-	const { mutate: editMessageMutation } = useMutation({
-		mutationFn: async ({
-			chatID,
-			messageID,
-			editedMessage
-		}: {
-			chatID: string;
-			messageID: string;
-			editedMessage: string;
-		}) => {
-			try {
-				await axios.patch(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/${messageID}/edit-message`,
-					{
-						editedMessage
-					},
-					{
-						withCredentials: true
-					}
-				);
-			} catch (error) {
-				console.error(error);
-			}
-		},
-		onSuccess: () => {
-			queryClient.invalidateQueries({
-				queryKey: ["chat-messages", chatID]
-			});
-			queryClient.invalidateQueries({ queryKey: ["your-chats"] });
-		}
-	});
+  const { mutate: editMessageMutation } = useMutation({
+    mutationFn: async ({
+      chatID,
+      messageID,
+      editedMessage,
+    }: {
+      chatID: string;
+      messageID: string;
+      editedMessage: string;
+    }) => {
+      try {
+        await axios.patch(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/${messageID}/edit-message`,
+          {
+            editedMessage,
+          },
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["chat-messages", chatID],
+      });
+      queryClient.invalidateQueries({ queryKey: ["your-chats"] });
+    },
+  });
 
-	function editMessage(
-		chatID: string,
-		messageID: string,
-		editedMessage: string
-	) {
-		if (!editedMessage.trim()) return alert("Edited message cannot be empty");
+  function editMessage(
+    chatID: string,
+    messageID: string,
+    editedMessage: string,
+  ) {
+    if (!editedMessage.trim()) return alert("Edited message cannot be empty");
 
-		if(editedMessage.length > 2000) {
-			return alert("Edited message cannot exceed 2000 characters");
-		}
+    if (editedMessage.length > 2000) {
+      return alert("Edited message cannot exceed 2000 characters");
+    }
 
-		editMessageMutation({ chatID, messageID, editedMessage });
-	}
+    editMessageMutation({ chatID, messageID, editedMessage });
+  }
 
-	function endRolePlayConversation(chatID: string) {
-		const confirmation = confirm(
-			"WARNING! Are you sure you want to end this role-play chat? This action cannot be undone and will end the chat for both you and your partner!"
-		);
+  const { mutate: pinMessageMutation } = useMutation({
+    mutationFn: async ({
+      chatID,
+      messageID,
+    }: {
+      chatID: string;
+      messageID: string;
+    }) => {
+      try {
+        await axios.patch(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/${messageID}/pin`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["chat-messages", chatID],
+      });
+      queryClient.invalidateQueries({ queryKey: ["your-chats"] });
+    },
+  });
 
-		if (!confirmation) return;
-		endChatMutation({ chatID });
-	}
+  function endRolePlayConversation(chatID: string) {
+    const confirmation = confirm(
+      "WARNING! Are you sure you want to end this role-play chat? This action cannot be undone and will end the chat for both you and your partner!",
+    );
 
-	const { mutate: removeChatFromListMutation } = useMutation({
-		mutationFn: async ({ chatID }: { chatID: string }) => {
-			try {
-				await axios.patch(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/remove-from-list`,
-					{},
-					{
-						withCredentials: true
-					}
-				);
-			} catch (error) {
-				console.error(error);
-			}
-		},
-		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: ["your-chats"] });
-			navigate("/inbox");
-		}
-	});
+    if (!confirmation) return;
+    endChatMutation({ chatID });
+  }
 
-	function removeChatFromList(chatID: string) {
-		const confirmation = confirm(
-			"Are you sure you want to remove this chat from your inbox? You can still access it if your partner sends a new message"
-		);
-		// TODO - see what happens when you hit the button on the roleplay ad you removed the chat for
-		if (!confirmation) return;
+  const { mutate: removeChatFromListMutation } = useMutation({
+    mutationFn: async ({ chatID }: { chatID: string }) => {
+      try {
+        await axios.patch(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/remove-from-list`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["your-chats"] });
+      navigate("/inbox");
+    },
+  });
 
-		removeChatFromListMutation({ chatID });
-	}
+  function removeChatFromList(chatID: string) {
+    const confirmation = confirm(
+      "Are you sure you want to remove this chat from your inbox? You can still access it if your partner sends a new message",
+    );
+    // TODO - see what happens when you hit the button on the roleplay ad you removed the chat for
+    if (!confirmation) return;
 
-	return {
-		createConversation,
-		rolePlayChats,
-		sendMessage,
-		rolePlayChatMessages,
-		deleteMessage,
-		editMessage,
-		endRolePlayConversation,
-		removeChatFromList,
-		currUserConversations
-	};
+    removeChatFromListMutation({ chatID });
+  }
+
+  return {
+    createConversation,
+    rolePlayChats,
+    sendMessage,
+    rolePlayChatMessages,
+    deleteMessage,
+    editMessage,
+    endRolePlayConversation,
+    removeChatFromList,
+    currUserConversations,
+    pinMessageMutation,
+  };
 }
