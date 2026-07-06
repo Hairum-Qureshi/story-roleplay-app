628e1c35f3caacda36a03481d662623944dfd68f
Hairum Qureshi <hqureshi@udel.edu>
2026-07-06 10:46:26 -0400

    updated import for Like schema and also removed unnecessary assertions

--- apps/backend/src/role-play-ad/role-play-ad.service.ts
+++ apps/backend/src/role-play-ad/role-play-ad.service.ts
@@ -9,8 +9,8 @@ import { ConversationDocument, Message, UserPayload } from '../types';
 import { Conversation } from '../schemas/inbox/Conversation';
 import { ChatService } from '../chat/chat.service';
 import mongoose from 'mongoose';
-import { Like } from 'src/schemas/Like';
 import { RolePlayAd as RolePlayAdType } from '../types';
+import { Like } from '../schemas/Like';
 
 @Injectable()
 export class RolePlayAdService {
@@ -110,14 +110,14 @@ export class RolePlayAdService {
     return ads.map((ad) => ({
       ...ad,
       canBeReposted: now - new Date(ad.createdAt).getTime() >= ONE_HOUR,
-    })) as unknown as (RolePlayAd & { canBeReposted: boolean })[];
+    }));
   }
 
   async getAllAds(userID: string) {
     // ! there seems to be an issue where if you repost more than 1 ad, then the frontend UI appears to duplicate the ad more than once unless you refresh the page
     const ONE_HOUR = 60 * 60 * 1000;
 
-    const allAds: RolePlayAdDocument[] = (await this.rolePlayAdModel
+    const allAds: RolePlayAdDocument[] = await this.rolePlayAdModel
       .find({
         createdAt: {
           $gte: new Date(Date.now() - ONE_HOUR),
@@ -129,7 +129,7 @@ export class RolePlayAdService {
         select: 'username profilePicture',
       })
       .select('-__v')
-      .sort({ createdAt: -1 })) as unknown as RolePlayAdDocument[];
+      .sort({ createdAt: -1 });
 
     const allAdsJSON: RolePlayAdType[] = await Promise.all(
       allAds.map(async (ad) => {
