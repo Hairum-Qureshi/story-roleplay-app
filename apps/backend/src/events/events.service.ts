8f67764fc702b6f12768e6612b69921375e0b5dc
Hairum Qureshi <hqureshi@udel.edu>
2026-07-09 11:14:45 -0400

    renamed 'viewMap' method to 'viewSocketToUserMap'

--- apps/backend/src/events/events.service.ts
+++ apps/backend/src/events/events.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class EventsService {
   private socketToUserMap: Map<string, string> = new Map<string, string>();
+  private notesEditorMap: Map<string, string> = new Map<string, string>();
 
   identifyUser(socketID: string, userID: string) {
     this.socketToUserMap.set(socketID, userID);
   }
 
-  viewMap(): Map<string, string> {
+  viewSocketToUserMap(): Map<string, string> {
     return this.socketToUserMap;
   }
 
