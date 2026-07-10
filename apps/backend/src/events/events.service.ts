62a65a814e137c4cae8b5e7ed731425d94c3b88f
Hairum Qureshi <hqureshi@udel.edu>
2026-07-10 01:53:24 -0400

    created helper methods for handling checks in editor hashmap

--- apps/backend/src/events/events.service.ts
+++ apps/backend/src/events/events.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Editor } from 'src/types';
 
 @Injectable()
 export class EventsService {
   private socketToUserMap: Map<string, string> = new Map<string, string>();
-  private notesEditorMap: Map<string, string> = new Map<string, string>();
+  private notesEditorMap: Map<string, Editor> = new Map<string, Editor>();
 
   identifyUser(socketID: string, userID: string) {
     this.socketToUserMap.set(socketID, userID);
@@ -11,6 +12,32 @@ export class EventsService {
 
   viewSocketToUserMap(): Map<string, string> {
     return this.socketToUserMap;
+  }
+
+  addUserToNotesEditorMap(
+    userID: string,
+    chatID: string,
+    username: string,
+  ): Editor {
+    const existingEditor = this.notesEditorMap.get(chatID);
+
+    if (existingEditor) {
+      return existingEditor;
+    }
+
+    const newEditor = { userID, username };
+    this.notesEditorMap.set(chatID, newEditor);
+    console.log(`User ${userID} is now editing a note for chat ${chatID}`);
+
+    return newEditor;
+  }
+
+  viewNotesEditorMap(): Map<string, Editor> {
+    return this.notesEditorMap;
+  }
+
+  getNotesEditor(chatID: string): Editor | undefined {
+    return this.notesEditorMap.get(chatID);
   }
 
   removeUserBySocketId(socketID: string) {
