43d99f5ba258125505d5f90205657f3d8d2a0288
Hairum Qureshi <hqureshi@udel.edu>
2026-07-09 23:58:34 -0400

    registered isChatMemberGuard as a provider and also imported Mongoose Conversation Schema

--- apps/backend/src/events/events.module.ts
+++ apps/backend/src/events/events.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
 import { EventsService } from './events.service';
+import { IsChatMemberGuard } from 'src/guards/websockets/isChatMember.guard';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Conversation,
+  ConversationSchema,
+} from 'src/schemas/inbox/Conversation';
 
 @Module({
-  providers: [EventsGateway, EventsService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Conversation.name, schema: ConversationSchema },
+    ]),
+  ],
+  providers: [EventsGateway, EventsService, IsChatMemberGuard],
   exports: [EventsGateway],
 })
 export class EventsModule {}
