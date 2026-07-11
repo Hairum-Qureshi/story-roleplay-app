4408c7f3614a64cdaafd139481593d6cfa52f6d6
Hairum Qureshi <hqureshi@udel.edu>
2026-07-10 23:50:20 -0400

    updated useChatStore to handle keeping track of the selected chat as well as setting the selected chat

--- apps/frontend/src/store/useChatStore.ts
+++ apps/frontend/src/store/useChatStore.ts
@@ -1,13 +1,32 @@
 import { create } from "zustand";
+import type { Conversation } from "../interfaces";
 
 interface ChatStore {
   hideSystemMessages: boolean;
+  selectedChat: Conversation | null;
   setHideSystemMessages: (hide: boolean) => void;
+  setSelectedChat: (chat: Conversation | null) => void;
+  syncSelectedChatById: (
+    chatID: string | undefined,
+    chats: Conversation[] | undefined,
+  ) => void;
 }
 
-const useChatStore = create<ChatStore>((set, get) => ({
+const useChatStore = create<ChatStore>((set) => ({
   hideSystemMessages: false,
+  selectedChat: null,
   setHideSystemMessages: (hide: boolean) => set({ hideSystemMessages: hide }),
+  setSelectedChat: (chat: Conversation | null) => set({ selectedChat: chat }),
+  syncSelectedChatById: (
+    chatID: string | undefined,
+    chats: Conversation[] | undefined,
+  ) => {
+    const chat = chatID
+      ? chats?.find((conversation) => conversation._id === chatID) || null
+      : null;
+
+    set({ selectedChat: chat });
+  },
 }));
 
 export default useChatStore;
