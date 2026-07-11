edf7cbfcd3421486f848c1baae7d6e991a6fb321
Hairum Qureshi <hqureshi@udel.edu>
2026-07-11 02:22:32 -0400

    removed unused Navigate component

--- apps/frontend/src/components/middleware/OwnerRoutesGuard.tsx
+++ apps/frontend/src/components/middleware/OwnerRoutesGuard.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { Navigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useCurrentUser } from "../../hooks/useCurrentUser";
 import useRolePlayAds from "../../hooks/useRolePlayAds";
 import NotFound from "../../pages/NotFound";
 
 export default function OwnerRoutesGuard({
-	children
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	const { data: currUserData } = useCurrentUser();
-	const { adID } = useParams();
-	const { adData, loading: adLoading } = useRolePlayAds(adID);
+  const { data: currUserData } = useCurrentUser();
+  const { adID } = useParams();
+  const { adData, loading: adLoading } = useRolePlayAds(adID);
 
-	if (adLoading) {
-		return null;
-	}
+  if (adLoading) {
+    return null;
+  }
 
-	if (!adData || adData.author._id !== currUserData?._id) {
-		return <NotFound />;
-	}
+  if (!adData || adData.author._id !== currUserData?._id) {
+    return <NotFound />;
+  }
 
-	return <>{children}</>;
+  return <>{children}</>;
 }
