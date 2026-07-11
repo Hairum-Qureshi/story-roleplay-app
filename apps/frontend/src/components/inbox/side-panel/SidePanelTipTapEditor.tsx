d89cc9ca602af52646522c83792a685bfcb0aff9
Hairum Qureshi <hqureshi@udel.edu>
2026-07-11 00:21:12 -0400

    added logic for showing a loading animation when text from the editor is being saved

--- apps/frontend/src/components/inbox/side-panel/SidePanelTipTapEditor.tsx
+++ apps/frontend/src/components/inbox/side-panel/SidePanelTipTapEditor.tsx
@@ -2,18 +2,40 @@ import TipTapEditor from "../../tiptap/TipTapEditor";
 import useSocketStore from "../../../store/useSocketStore";
 import { useCurrentUser } from "../../../hooks/useCurrentUser";
 import { useParams } from "react-router-dom";
+import { ring2 } from "ldrs";
+import { autoSaveStore } from "../../../store/useAutoSaveStore";
 
 export default function SidePanelTipTapEditor() {
   const { editorUsername, chatID: editorChatID } = useSocketStore();
   const { data: currentUser } = useCurrentUser();
   const { chatID } = useParams<{ chatID: string }>();
+  const { saving } = autoSaveStore();
+
+  ring2.register();
 
   return (
     <>
       <div className="px-5 pt-6 pb-4 border-b border-slate-800">
-        <h2 className="text-lg font-semibold text-white mt-5">
-          Role-Play Notes
-        </h2>
+        <div className="flex items-center justify-between">
+          <h2 className="text-lg font-semibold text-white mt-5">
+            Role-Play Notes
+          </h2>
+          <div className="flex items-center gap-2 mt-3">
+            {saving && (
+              <div className="text-sm text-slate-400 flex items-center gap-2">
+                <l-ring-2
+                  size="20"
+                  stroke="2"
+                  stroke-length="0.25"
+                  bg-opacity="0.1"
+                  speed="0.8"
+                  color="white"
+                ></l-ring-2>
+                Saving...
+              </div>
+            )}
+          </div>
+        </div>
         {editorUsername && editorChatID === chatID && (
           <div className="flex items-start gap-2">
             <div className="flex items-center gap-1">
