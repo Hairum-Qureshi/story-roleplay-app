282ea8c4f468bb08055bc17c00adc029399a3851
Hairum Qureshi <hqureshi@udel.edu>
2026-07-10 23:55:09 -0400

    updated import for useAtoSaveStore after renaming it and also updated backend PATCH endpoint for saving role-play notes

--- apps/frontend/src/hooks/useAutoSave.ts
+++ apps/frontend/src/hooks/useAutoSave.ts
@@ -1,10 +1,37 @@
 import { useRef } from "react";
-import { autoSaveStore } from "../store/autoSaveStore";
+import { autoSaveStore } from "../store/useAutoSaveStore";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
 
 export default function useAutoSave() {
   const setSaving = autoSaveStore((s) => s.setSaving);
   const { saving } = autoSaveStore();
   const keyUpTimer = useRef<number | null>(null);
+  const { chatID } = useParams();
+
+  const { mutate: postRoleplayAdMutate } = useMutation({
+    mutationFn: async ({ content }: { content: string }) => {
+      try {
+        const response = await axios.patch(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/chat/${chatID}/notes`,
+          {
+            content,
+          },
+          {
+            withCredentials: true,
+          },
+        );
+
+        return response;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      console.log("Note saved successfully");
+    },
+  });
 
   function autosave(noteData: string) {
     setSaving(true);
@@ -14,7 +41,9 @@ export default function useAutoSave() {
 
     keyUpTimer.current = window.setTimeout(() => {
       // code responsible for sending data to backend
-      console.log(noteData);
+
+      postRoleplayAdMutate({ content: noteData });
+
       setSaving(false);
     }, 500);
   }
