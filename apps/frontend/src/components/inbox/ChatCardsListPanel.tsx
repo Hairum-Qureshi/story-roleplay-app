fe1db75c4b9d4d02a2c5bab5eb78eef48fb37598
Hairum Qureshi <hqureshi@udel.edu>
2026-07-11 19:46:41 -0400

    added search bar

--- apps/frontend/src/components/inbox/ChatCardsListPanel.tsx
+++ apps/frontend/src/components/inbox/ChatCardsListPanel.tsx
@@ -3,6 +3,7 @@ import ChatContainer from "./ChatContainer";
 import type { Conversation } from "../../interfaces";
 import useChatStore from "../../store/useChatStore";
 import useSocketStore from "../../store/useSocketStore";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function ChatCardsListPanel({
   currUserConversations,
@@ -15,6 +16,16 @@ export default function ChatCardsListPanel({
 
   return (
     <div className="w-1/4 h-full min-h-0 pt-3 overflow-y-auto border-r border-slate-700 flex flex-col gap-1">
+      <div className="px-4 mb-2 relative">
+        <input
+          type="text"
+          placeholder="Search chats by title..."
+          className="w-full pl-10 pr-4 py-2 rounded-md bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <div className="absolute left-7 top-1/2 transform -translate-y-1/2 text-gray-400">
+          <FaMagnifyingGlass />
+        </div>
+      </div>
       {(currUserConversations?.conversations?.length ?? 0) > 0 ? (
         currUserConversations?.conversations.map((chat: Conversation) => (
           <div key={chat._id}>
