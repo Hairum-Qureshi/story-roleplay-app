7e2ad914279b1e45fa225196b6cbed2aceee377d
Hairum Qureshi <hqureshi@udel.edu>
2026-07-10 22:23:22 -0400

    added conditional logic to only emit if the uid that corresponds to the chatID in the notesEditorMap is the same as the uid in the payload

--- apps/backend/src/events/events.gateway.ts
+++ apps/backend/src/events/events.gateway.ts
@@ -173,22 +173,25 @@ export class EventsGateway {
         });
       }
 
-      const content = `@${username} has stopped editing notes for this role-play. To view changes, open the notes tab in the side panel.`;
+      // only emit if the uid that corresponds to the chatID in the notesEditorMap is the same as the uid in the payload
+      if (existingEditor && existingEditor.userID === uid) {
+        const content = `@${username} has stopped editing notes for this role-play. To view changes, open the notes tab in the side panel.`;
 
-      const systemMessage = await this.messageModel.create({
-        sender: '000000000000000000000001',
-        conversation: new Types.ObjectId(chatID),
-        content,
-      });
+        const systemMessage = await this.messageModel.create({
+          sender: '000000000000000000000001',
+          conversation: new Types.ObjectId(chatID),
+          content,
+        });
 
-      await this.conversationModel.findByIdAndUpdate(
-        new Types.ObjectId(chatID),
-        {
-          $push: { messages: systemMessage._id },
-        },
-      );
+        await this.conversationModel.findByIdAndUpdate(
+          new Types.ObjectId(chatID),
+          {
+            $push: { messages: systemMessage._id },
+          },
+        );
 
-      this.emitSystemMessage(chatID, content);
+        this.emitSystemMessage(chatID, content);
+      }
     }
 
     return;
