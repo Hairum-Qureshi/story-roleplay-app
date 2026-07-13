18e341bf75e93a69068480f51420d7438085556f
Hairum Qureshi <hqureshi@udel.edu>
2026-07-13 09:36:16 -0400

    registered Conversation Schema to imports array

--- apps/backend/src/notification/notification.module.ts
+++ apps/backend/src/notification/notification.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
 import { Notification, NotificationSchema } from '../schemas/Notification';
+import {
+  Conversation,
+  ConversationSchema,
+} from '../schemas/inbox/Conversation';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -10,6 +14,7 @@ import { AuthModule } from '../auth/auth.module';
     AuthModule,
     MongooseModule.forFeature([
       { name: Notification.name, schema: NotificationSchema },
+      { name: Conversation.name, schema: ConversationSchema },
     ]),
   ],
   providers: [NotificationService],
