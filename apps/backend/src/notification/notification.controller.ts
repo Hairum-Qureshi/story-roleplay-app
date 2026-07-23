ca9d1fc0a8c826b9209b8d4b3adc1969b385bf86
Hairum Qureshi <hqureshi@udel.edu>
2026-07-22 20:46:15 -0400

    updated route endpoints

--- apps/backend/src/notification/notification.controller.ts
+++ apps/backend/src/notification/notification.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Param, Patch } from '@nestjs/common';
+import type { UserPayload } from 'src/types';
+import { AuthGuard } from '@nestjs/passport';
 import { NotificationService } from './notification.service';
+import { CurrentUser } from 'src/decorators/currentUser.decorator';
 
 @Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
+
+  @Post(':chatID/new')
+  @UseGuards(AuthGuard())
+  createNotification(
+    @Param('chatID') chatID: string,
+    @CurrentUser() user: UserPayload,
+  ) {
+    return this.notificationService.createNotification(chatID, user._id);
+  }
+
+  @Patch(':chatID/reset/unread-count')
+  @UseGuards(AuthGuard())
+  resetUnreadCount(
+    @Param('chatID') chatID: string,
+    @CurrentUser() user: UserPayload,
+  ) {
+    return this.notificationService.resetUnreadCount(chatID, user._id);
+  }
 }
