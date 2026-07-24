17531261318e3ba9ca0df6a7be1d5e0d36eb808d
Hairum Qureshi <hqureshi@udel.edu>
2026-07-23 20:16:23 -0400

    removed question about browser notifications as it's no longer applicable after removing it

--- apps/frontend/src/pages/FAQ.tsx
+++ apps/frontend/src/pages/FAQ.tsx
@@ -118,12 +118,6 @@ export default function FAQ() {
             "No. Once pressed, the action is permanent. A new role-play session must be initiated to continue.",
         },
         {
-          question:
-            "How do I stop receiving browser notifications when I receive a new message?",
-          answer:
-            "To stop receiving browser notifications, you can adjust your browser settings to block notifications from this site. The exact steps may vary depending on your browser, but generally, you can go to your browser's settings or preferences, find the section for notifications or site permissions, and remove or block this site from sending notifications.",
-        },
-        {
           question: "What's the note feature for?",
           answer:
             "The note feature allows you to write and save role-play notes that can be shared with your role-play partner. This is useful for keeping track of character details, plot points, or any other information relevant to your ongoing role-play. Please avoid sharing sensitive personal information in the notes that you wouldn't want your role-play partner to see, as they will have access to the notes as well. Also, once a chat has ended, any notes you and your partner have written will remain permanenty within the chat.",
