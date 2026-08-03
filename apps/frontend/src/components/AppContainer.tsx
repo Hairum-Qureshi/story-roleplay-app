0533778e59002123e84120f961c179502a4d0d30
Hairum Qureshi <hqureshi@udel.edu>
2026-08-02 22:24:55 -0400

    created AppContainer component for handling notification count logic

---
+++ apps/frontend/src/components/AppContainer.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+
+export default function AppContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const location = useLocation();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    queryClient.refetchQueries({
+      queryKey: ["total-notifications"],
+    });
+
+    console.log("RAN");
+  }, [location.pathname]);
+
+  return <div className="overflow-y-hidden">{children}</div>;
+}
