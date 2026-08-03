6407cf5f67aa8d5d08819815486dc2b01afdce6e
Hairum Qureshi <hqureshi@udel.edu>
2026-08-02 22:44:10 -0400

    removed test function endpoint call for debugging

--- apps/frontend/src/components/AppContainer.tsx
+++ apps/frontend/src/components/AppContainer.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
 
 export default function AppContainer({
   children,
@@ -12,20 +11,10 @@ export default function AppContainer({
   const queryClient = useQueryClient();
 
   useEffect(() => {
-    // queryClient.refetchQueries({
-    //   queryKey: ["total-notifications"],
-    // });
+    queryClient.refetchQueries({
+      queryKey: ["total-notifications"],
+    }); 
 
-    async function test() {
-      await axios.get(
-        `${import.meta.env.VITE_BACKEND_BASE_URL}/notification/all/total`,
-        {
-          withCredentials: true,
-        },
-      );
-    }
-
-    test();
   }, [location.pathname]);
 
   return <div className="overflow-y-hidden">{children}</div>;
