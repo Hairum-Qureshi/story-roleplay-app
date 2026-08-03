312f686aae9590a9f2a33355ac160e932d1c38ee
Hairum Qureshi <hqureshi@udel.edu>
2026-08-03 11:56:01 -0400

    added HttpModule to imports array

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { User, UserSchema } from '../schemas/User';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt-strategy';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { JwtStrategy } from './jwt-strategy';
         schema: UserSchema,
       },
     ]),
+    HttpModule,
   ],
   providers: [AuthService, JwtStrategy, GoogleOAuthConfig],
   controllers: [AuthController],
