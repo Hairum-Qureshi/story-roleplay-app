0af5650aab669354f2139c8203d39f7206e23ba3
Hairum Qureshi <hqureshi@udel.edu>
2026-08-06 00:24:04 -0400

    created block and unblock mutations for calling respected endpoints

--- apps/frontend/src/hooks/useUser.ts
+++ apps/frontend/src/hooks/useUser.ts
@@ -3,35 +3,79 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 interface UseUserHook {
-	deleteProfile: () => void;
+  deleteProfile: () => void;
+  blockUserMutation: (targetUserId: string) => void;
+  unBlockUserMutation: (targetUserId: string) => void;
 }
 
 export default function useUser(): UseUserHook {
-	const queryClient = useQueryClient();
-	const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-	const { mutate } = useMutation({
-		mutationFn: async () => {
-			try {
-				await axios.delete(
-					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/delete-account`,
-					{
-						withCredentials: true
-					}
-				);
-			} catch (error) {
-				console.error(error);
-			}
-		},
-		onSuccess: () => {
-			queryClient.setQueryData(["currentUser"], null);
-			navigate("/");
-		}
-	});
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      try {
+        await axios.delete(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/delete-account`,
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.setQueryData(["currentUser"], null);
+      navigate("/");
+    },
+  });
 
-	function deleteProfile() {
-		mutate();
-	}
+  const { mutate: blockUserMutation } = useMutation({
+    mutationFn: async (targetUserId: string) => {
+      try {
+        await axios.post(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/${targetUserId}/block`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["currentUser"],
+      });
+    },
+  });
 
-	return { deleteProfile };
+  const { mutate: unBlockUserMutation } = useMutation({
+    mutationFn: async (targetUserId: string) => {
+      try {
+        await axios.post(
+          `${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/${targetUserId}/unblock`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["currentUser"],
+      });
+    },
+  });
+
+  function deleteProfile() {
+    mutate();
+  }
+
+  return { deleteProfile, blockUserMutation, unBlockUserMutation };
 }
