4c074f5f6e5c240485f01c0cbc916fe7ec9b3693
Hairum Qureshi <hqureshi@udel.edu>
2026-08-07 20:27:38 -0400

    created ReportModal component that opens up a basic template modal along with a closing functionality

---
+++ apps/frontend/src/components/ReportModal.tsx
@@ -0,0 +1,23 @@
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
+
+export default function ReportModal({
+  openModal,
+  onCloseModal,
+}: {
+  openModal: boolean;
+  onCloseModal: () => void;
+}) {
+  return (
+    <div>
+      <Modal open={openModal} onClose={() => onCloseModal()} center>
+        <h2>Simple centered modal</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+          hendrerit risus, sed porttitor quam.
+        </p>
+      </Modal>
+    </div>
+  );
+}
