97a3e9d7b5c768ad4880854b4fb9f42105bfae8c
Hairum Qureshi <hqureshi@udel.edu>
2026-08-08 16:44:22 -0400

    removed AuthModule from imports array

--- apps/backend/src/report/report.module.ts
+++ apps/backend/src/report/report.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
-import { AuthModule } from 'src/auth/auth.module';
 import { UserSchema } from 'src/schemas/User';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   providers: [ReportService],
-  imports: [
-    AuthModule,
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-  ],
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [ReportController],
+  exports: [ReportService],
 })
 export class ReportModule {}
