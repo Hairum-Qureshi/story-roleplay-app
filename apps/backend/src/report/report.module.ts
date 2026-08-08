50a35c6e042b0b19b02ebc7d478097fc2526f61c
Hairum Qureshi <hqureshi@udel.edu>
2026-08-08 00:33:11 -0400

    imported AuthModule

--- apps/backend/src/report/report.module.ts
+++ apps/backend/src/report/report.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [ReportService],
-  controllers: [ReportController]
+  imports: [AuthModule],
+  controllers: [ReportController],
 })
 export class ReportModule {}
