dced80b1e9b9caa9202464560a1051a2c36b7f91
Hairum Qureshi <hqureshi@udel.edu>
2026-08-08 16:45:21 -0400

    moved enums to enums folder and imported them

---
+++ apps/backend/src/schemas/Report.ts
@@ -0,0 +1,45 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { RolePlayAd, RolePlayAdSchema } from './RolePlayAd';
+import { ReportReason } from 'src/enums/reason.enum';
+import { AdStatus } from 'src/enums/adStatus.enum';
+
+@Schema()
+export class Report {
+  @Prop({ ref: 'User' })
+  reporterUserID: string;
+
+  @Prop({ ref: 'User' })
+  reportedUserID: string;
+
+  @Prop({ required: true, enum: ReportReason })
+  reason: ReportReason;
+
+  @Prop({ type: RolePlayAdSchema })
+  snapshot: RolePlayAd;
+
+  @Prop({ type: String, required: true })
+  adID: string;
+
+  @Prop({ type: String })
+  reportDetails: string;
+
+  @Prop({ type: String })
+  originalAdPoster: string;
+
+  @Prop({ type: String })
+  reportNotes: string;
+
+  @Prop({ required: true, enum: AdStatus, default: AdStatus.OPEN })
+  status: AdStatus;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+}
+
+SchemaFactory.createForClass(Report);
+export const ReportSchema = SchemaFactory.createForClass(Report);
+export type ReportDocument = HydratedDocument<Report>;
