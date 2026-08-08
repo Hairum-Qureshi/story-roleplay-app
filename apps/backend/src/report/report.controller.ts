2a2849b860ce51d778098239f2cb064afa48ff84
Hairum Qureshi <hqureshi@udel.edu>
2026-08-08 01:33:59 -0400

    created all controller endpoints related to reporting a post

--- apps/backend/src/report/report.controller.ts
+++ apps/backend/src/report/report.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ReportService } from './report.service';
+import { AuthGuard } from '@nestjs/passport';
+import { HasRolePermissions } from 'src/guards/isAuthorized.guard';
+import Role from 'src/roles.enum';
+import { Roles } from 'src/decorators/roles.decorator';
 
-@Controller('report')
-export class ReportController {}
+@Controller('api/report')
+export class ReportController {
+  constructor(private reportService: ReportService) {}
+
+  @Get('all')
+  @UseGuards(AuthGuard(), HasRolePermissions)
+  @Roles([Role.ADMIN, Role.MODERATOR])
+  getAllReports(@Query('status') status?: 'OPEN' | 'CLOSED' | 'RESOLVED') {}
+
+  @Get(':reportID')
+  @UseGuards(AuthGuard(), HasRolePermissions)
+  @Roles([Role.ADMIN, Role.MODERATOR])
+  getReport() {}
+
+  @Post(':reportID/notes')
+  @UseGuards(AuthGuard(), HasRolePermissions)
+  @Roles([Role.ADMIN, Role.MODERATOR])
+  addReportNotes() {}
+
+  @Post('new')
+  @UseGuards(AuthGuard())
+  createReport() {}
+
+  @Post(':reportID/resolve')
+  @UseGuards(AuthGuard(), HasRolePermissions)
+  @Roles([Role.ADMIN, Role.MODERATOR])
+  resolveReport() {}
+
+  @Post(':reportID/reopen')
+  @UseGuards(AuthGuard(), HasRolePermissions)
+  @Roles([Role.ADMIN, Role.MODERATOR])
+  reopenReport() {}
+
+  @Post(':reportID/close')
+  @UseGuards(AuthGuard(), HasRolePermissions)
+  @Roles([Role.ADMIN, Role.MODERATOR])
+  closeReport() {}
+}
