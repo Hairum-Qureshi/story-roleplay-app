5d67364cfdcf88dcad499ae00c72d3a14f8b6749
Hairum Qureshi <hqureshi@udel.edu>
2026-08-10 23:34:01 -0400

    imported ReportSchema

--- apps/backend/src/report/report.module.ts
+++ apps/backend/src/report/report.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
 import { UserSchema } from 'src/schemas/User';
+import { ReportSchema } from 'src/schemas/Report';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   providers: [ReportService],
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: 'Report', schema: ReportSchema }]),
+  ],
   controllers: [ReportController],
   exports: [ReportService],
 })
