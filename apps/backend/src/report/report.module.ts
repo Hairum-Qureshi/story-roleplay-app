b2eed7f45f53d54ed039d7e46d21aa6c61b1aa6f
Hairum Qureshi <hqureshi@udel.edu>
2026-08-10 21:01:05 -0400

    re-importing AuthModule after resolving circular dependency error

--- apps/backend/src/report/report.module.ts
+++ apps/backend/src/report/report.module.ts
@@ -3,10 +3,14 @@ import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
 import { UserSchema } from 'src/schemas/User';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [ReportService],
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
   controllers: [ReportController],
   exports: [ReportService],
 })
