008bf58c3728df13fc6cf3ff5c7e05e29398c0e1
Hairum Qureshi <hqureshi@udel.edu>
2026-08-11 14:34:59 -0400

    created getAllReports method and and createReport method for retrieving reports and creating them

--- apps/backend/src/report/report.service.ts
+++ apps/backend/src/report/report.service.ts
@@ -1,12 +1,88 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { ModerationStatus } from 'src/enums/moderation.enum';
+import { ReportDocument } from 'src/schemas/Report';
+import { CreateReport } from 'src/DTOs/CreateReport.dto';
 import { User, UserDocument } from 'src/schemas/User';
+import { ReportMap, RolePlayAd } from 'src/types';
+import { ReportReason } from 'src/enums/reason.enum';
+import { AdStatus } from 'src/enums/adStatus.enum';
 
 @Injectable()
 export class ReportService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel('Report') private reportModel: Model<ReportDocument>,
+    @InjectModel('RolePlayAd') private rolePlayAdModel: Model<RolePlayAd>,
+  ) {}
 
- 
+  private async getAdByID(adID: string): Promise<RolePlayAd | null> {
+    const ad = await this.rolePlayAdModel.findById(adID).lean();
+
+    if (!ad) throw new NotFoundException(`Ad with ID ${adID} not found`);
+
+    return ad;
+  }
+
+  async getAllReports(status?: 'OPEN' | 'CLOSED' | 'RESOLVED') {
+    // TODO - later implement pagination
+    return !status
+      ? await this.reportModel.find({})
+      : await this.reportModel.find({ status: AdStatus[status.toUpperCase()] });
+  }
+
+  async createReport(reportData: CreateReport, currUserID: string) {
+    const { reason, reportDetails, adLink, claimedOriginalAdPoster } =
+      reportData;
+
+    const adID: string = adLink.split('/').pop()!;
+
+    const ad = await this.getAdByID(adID);
+
+    if (!ad) return;
+
+    delete ad['__v'];
+
+    if (ad.author === currUserID)
+      throw new HttpException(
+        'You cannot report your own ad',
+        HttpStatus.BAD_REQUEST,
+      );
+
+    const reportedAd = await this.reportModel.findOne({
+      reporterUserID: currUserID,
+    });
+
+    if (reportedAd)
+      throw new HttpException(
+        'You have already reported this ad',
+        HttpStatus.BAD_REQUEST,
+      );
+
+    const reasonMap: ReportMap = {
+      'Underage or minor-related sexual content': ReportReason.Underage,
+      'Purely sexual or smut-focused ad': ReportReason.SexualContent,
+      'Hate speech, slurs, or extremist content': ReportReason.HateSpeech,
+      'This ad is plagiarized or stolen content': ReportReason.Plagiarism,
+      'Doxxing or sharing personal/contact information': ReportReason.Doxxing,
+      'Unrelated content or spam': ReportReason.Spam,
+      'AI generated content': ReportReason.AIContent,
+      Other: ReportReason.Other,
+    };
+
+    await this.reportModel.create({
+      reporterUserID: currUserID,
+      reason: reasonMap[reason],
+      adSnapshot: ad,
+      adID,
+      reportDetails,
+      reportedUserID: claimedOriginalAdPoster || ad.author,
+      adLink,
+    });
+  }
 }
