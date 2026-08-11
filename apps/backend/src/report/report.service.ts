c931fb96011bc2024ea238df22035919db86f27d
Hairum Qureshi <hqureshi@udel.edu>
2026-08-10 20:59:36 -0400

    moved 'checkUserModerationStatus' method to moderation service file

--- apps/backend/src/report/report.service.ts
+++ apps/backend/src/report/report.service.ts
@@ -8,25 +8,5 @@ import { User, UserDocument } from 'src/schemas/User';
 export class ReportService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async checkUserModerationStatus(user: UserDocument) {
-    if (user.moderation.status === ModerationStatus.BANNED) {
-      throw new HttpException('User is banned', 403);
-    }
-
-    if (user.moderation.status === ModerationStatus.SUSPENDED) {
-      const now = new Date();
-      if (user.moderation.expiresAt && user.moderation.expiresAt > now) {
-        throw new HttpException(
-          `User is suspended until ${user.moderation.expiresAt}`,
-          403,
-        );
-      } else {
-        // If the suspension has expired, reset the moderation status
-        user.moderation.status = ModerationStatus.NONE;
-        user.moderation.reason = '';
-        user.moderation.expiresAt = null;
-        await user.save();
-      }
-    }
-  }
+ 
 }
