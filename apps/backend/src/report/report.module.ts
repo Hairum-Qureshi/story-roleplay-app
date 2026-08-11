a22089bae6a370f3defada112800510e9856bcf1
Hairum Qureshi <hqureshi@udel.edu>
2026-08-10 23:39:40 -0400

    imported RolePlayAd schema

--- apps/backend/src/report/report.module.ts
+++ apps/backend/src/report/report.module.ts
@@ -4,12 +4,16 @@ import { ReportController } from './report.controller';
 import { UserSchema } from 'src/schemas/User';
 import { ReportSchema } from 'src/schemas/Report';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RolePlayAdSchema } from 'src/schemas/RolePlayAd';
 
 @Module({
   providers: [ReportService],
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     MongooseModule.forFeature([{ name: 'Report', schema: ReportSchema }]),
+    MongooseModule.forFeature([
+      { name: 'RolePlayAd', schema: RolePlayAdSchema },
+    ]),
   ],
   controllers: [ReportController],
   exports: [ReportService],
