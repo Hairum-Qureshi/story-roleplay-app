4627d4156a2c13a7e7d736d50d16946cd74dc460
Hairum Qureshi <hqureshi@udel.edu>
2026-08-12 19:48:32 -0400

    added proper error handling where errors are alerted to the user

--- apps/frontend/src/hooks/useReport.ts
+++ apps/frontend/src/hooks/useReport.ts
@@ -2,19 +2,19 @@ import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
 
 export default function useReport() {
-  const { mutate: createReportMutation } = useMutation({
-    mutationFn: async ({
-      reason,
-      reportDetails,
-      adLink,
-      claimedOriginalAdPoster,
-    }: {
-      reason: string;
-      reportDetails: string;
-      adLink: string;
-      claimedOriginalAdPoster: string;
-    }) => {
-      try {
+  const { mutate: createReportMutation, isPending: isReportLoading } =
+    useMutation({
+      mutationFn: async ({
+        reason,
+        reportDetails,
+        adLink,
+        claimedOriginalAdPoster,
+      }: {
+        reason: string;
+        reportDetails: string;
+        adLink: string;
+        claimedOriginalAdPoster: string;
+      }) => {
         const response = await axios.post(
           `${import.meta.env.VITE_BACKEND_BASE_URL}/api/report/create`,
           {
@@ -29,11 +29,15 @@ export default function useReport() {
         );
 
         return response;
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  });
+      },
+      onError: (error) => {
+        if (axios.isAxiosError(error)) {
+          alert(error.response?.data?.message);
+          return;
+        }
+        alert("Error reporting ad: " + error);
+      },
+    });
 
-  return { createReportMutation };
+  return { createReportMutation, isReportLoading };
 }
