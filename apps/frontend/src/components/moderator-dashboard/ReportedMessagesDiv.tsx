934b8d58518df245df3f4210279784eb97d73a30
Hairum Qureshi <hqureshi@udel.edu>
2026-08-13 09:54:04 -0400

    added ReportedMessageCard component

--- apps/frontend/src/components/moderator-dashboard/ReportedMessagesDiv.tsx
+++ apps/frontend/src/components/moderator-dashboard/ReportedMessagesDiv.tsx
@@ -1,7 +1,12 @@
+import ReportedMessageCard from "./ReportedMessageCard";
+
 export default function ReportedMessagesDiv() {
   return (
     <div className="bg-slate-900/80 p-5 w-1/2 rounded-md shadow-md text-white border border-sky-600/50 space-y-3">
       <h1 className="text-xl font-semibold">Reported Messages</h1>
+      <div>
+        <ReportedMessageCard />
+      </div>
     </div>
   );
 }
