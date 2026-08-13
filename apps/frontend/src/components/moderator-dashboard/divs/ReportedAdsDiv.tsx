7de2944a69b723ce85b7bdddaa452df835144371
Hairum Qureshi <hqureshi@udel.edu>
2026-08-13 13:39:32 -0400

    added logic for rendering a loading message when searching for reports, a message if no reports were found, and logic for rendering the ReportedAdCard component for each found report

--- apps/frontend/src/components/moderator-dashboard/divs/ReportedAdsDiv.tsx
+++ apps/frontend/src/components/moderator-dashboard/divs/ReportedAdsDiv.tsx
@@ -1,11 +1,27 @@
+import useReport from "../../../hooks/useReport";
+import type { ReportAd } from "../../../interfaces";
 import ReportedAdCard from "../cards/ReportedAdCard";
 
 export default function ReportedAdsDiv() {
+  const { allOpenReports, isAllReportsLoading } = useReport();
+
   return (
     <div className="bg-slate-900/80 p-5 w-1/2 rounded-md shadow-md text-white border border-sky-600/50 space-y-3">
       <h1 className="text-xl font-semibold">Reported Posts</h1>
       <div>
-        <ReportedAdCard />
+        {isAllReportsLoading ? (
+          <p className="text-center text-slate-400 my-10 text-lg">
+            Loading reports...
+          </p>
+        ) : !allOpenReports?.length ? (
+          <p className="text-center text-slate-400 my-10 text-lg">
+            No reports found.
+          </p>
+        ) : (
+          allOpenReports?.map((report: ReportAd) => (
+            <ReportedAdCard key={report._id} report={report} />
+          ))
+        )}
       </div>
     </div>
   );
