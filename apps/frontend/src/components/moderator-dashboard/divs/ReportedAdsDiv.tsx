c3f9a032457a769da360a4176d9af981b5ea6226
Hairum Qureshi <hqureshi@udel.edu>
2026-08-13 13:53:37 -0400

    changed 'allOpenReports' to 'allReports' and added a visible number of reports

--- apps/frontend/src/components/moderator-dashboard/divs/ReportedAdsDiv.tsx
+++ apps/frontend/src/components/moderator-dashboard/divs/ReportedAdsDiv.tsx
@@ -3,22 +3,22 @@ import type { ReportAd } from "../../../interfaces";
 import ReportedAdCard from "../cards/ReportedAdCard";
 
 export default function ReportedAdsDiv() {
-  const { allOpenReports, isAllReportsLoading } = useReport();
+  const { allReports, isAllReportsLoading } = useReport();
 
   return (
     <div className="bg-slate-900/80 p-5 w-1/2 rounded-md shadow-md text-white border border-sky-600/50 space-y-3">
-      <h1 className="text-xl font-semibold">Reported Posts</h1>
+      <h1 className="text-xl font-semibold">Reported Posts ({allReports?.length})</h1>
       <div>
         {isAllReportsLoading ? (
           <p className="text-center text-slate-400 my-10 text-lg">
             Loading reports...
           </p>
-        ) : !allOpenReports?.length ? (
+        ) : !allReports?.length ? (
           <p className="text-center text-slate-400 my-10 text-lg">
             No reports found.
           </p>
         ) : (
-          allOpenReports?.map((report: ReportAd) => (
+          allReports?.map((report: ReportAd) => (
             <ReportedAdCard key={report._id} report={report} />
           ))
         )}
