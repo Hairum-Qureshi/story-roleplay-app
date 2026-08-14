3a8afc95f79c5f8557797449324dccbf5bc533b7
Hairum Qureshi <hqureshi@udel.edu>
2026-08-13 23:31:25 -0400

    created new moderator protected POST endpoint for sending an apology email to the user that had been unfairy banned/suspended/warned

--- apps/backend/src/email/email.controller.ts
+++ apps/backend/src/email/email.controller.ts
@@ -5,6 +5,9 @@ import type { UserPayload } from '../types';
 import { SendEmail } from '../DTOs/SendEmail.dto';
 import { CurrentUser } from '../decorators/currentUser.decorator';
 import { ModerationGuard } from 'src/guards/moderation.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import Role from 'src/enums/roles.enum';
+import { ReportedUserEmailPayload } from 'src/DTOs/ReportedUserEmail.dto';
 
 @Controller('email')
 export class EmailController {
@@ -18,5 +21,18 @@ export class EmailController {
   ) {
     await this.emailService.sendEmail(sendEmailDto, currUser);
     return { message: 'Email sent successfully' };
+  }
+
+  @Post('/moderator-apology/send')
+  @UseGuards(AuthGuard(), ModerationGuard)
+  @Roles([Role.ADMIN, Role.MODERATOR])
+  async sendApologyModeratorEmail(
+    @CurrentUser() currUser: UserPayload,
+    @Body() reportedUserDataDto: ReportedUserEmailPayload,
+  ) {
+    await this.emailService.sendApologyModeratorEmail(
+      currUser,
+      reportedUserDataDto,
+    );
   }
 }
