36b61eb6d3d5b43ab127abef6e13b52013f30c38
Hairum Qureshi <hqureshi@udel.edu>
2026-08-13 21:11:31 -0400

    removed old values and replaced them with new setter data and variables including suspensionDuration, username, and name

--- apps/frontend/src/store/useModerationStore.ts
+++ apps/frontend/src/store/useModerationStore.ts
@@ -1,11 +1,21 @@
 import { create } from "zustand";
 
 interface ModerationState {
-  sentEmail: boolean;
-  setSentEmail: (value: boolean) => void;
+  suspensionDuration: number;
+  setSuspensionDuration: (duration: number) => void;
+  username: string;
+  setUsername: (username: string) => void;
+  name: string;
+  setName: (name: string) => void;
 }
 
 export const moderationStore = create<ModerationState>((set) => ({
-  sentEmail: false,
-  setSentEmail: (value: boolean) => set({ sentEmail: value }),
+  setSuspensionDuration: (duration: number) =>
+    set((state) => ({ ...state, suspensionDuration: duration })),
+  suspensionDuration: 7,
+  setUsername: (username: string) =>
+    set((state) => ({ ...state, username: username })),
+  username: "",
+  setName: (name: string) => set((state) => ({ ...state, name: name })),
+  name: "",
 }));
