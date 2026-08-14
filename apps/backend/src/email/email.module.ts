4f9a7e0c19cc11ba3dd33a0a509a22ca92c3299d
Hairum Qureshi <hqureshi@udel.edu>
2026-08-14 00:15:47 -0400

    registered ReportSchema

--- apps/backend/src/email/email.module.ts
+++ apps/backend/src/email/email.module.ts
@@ -3,12 +3,19 @@ import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../schemas/User';
+import { Report, ReportSchema } from 'src/schemas/Report';
 
 @Module({
   providers: [EmailService],
   controllers: [EmailController],
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      {
+        name: Report.name,
+        schema: ReportSchema,
+      },
+    ]),
   ],
 })
 export class EmailModule {}
