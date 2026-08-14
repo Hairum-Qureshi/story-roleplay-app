c5c9af02ab6ae7235ec84f4d78f8176482881e0c
Hairum Qureshi <hqureshi@udel.edu>
2026-08-13 23:31:44 -0400

    created sendApologyModeratorEmail method for sending apology email

--- apps/backend/src/email/email.service.ts
+++ apps/backend/src/email/email.service.ts
@@ -4,6 +4,7 @@ import { ProfanityEngine } from '@coffeeandfun/google-profanity-words';
 import { ConfigService } from '@nestjs/config';
 import { Resend } from 'resend';
 import { SendEmail } from '../DTOs/SendEmail.dto';
+import { ReportedUserEmailPayload } from 'src/DTOs/ReportedUserEmail.dto';
 
 @Injectable()
 export class EmailService {
@@ -42,6 +43,57 @@ export class EmailService {
       to: this.configService.get<string>('RECEIVER_EMAIL')!,
       subject: `[TaleWeaver Feedback] ${subject}`,
       text: `${message}\n\nFrom: ${from}`,
+    });
+  }
+
+  async sendApologyModeratorEmail(
+    currUser: UserPayload,
+    reportedUserDataDto: ReportedUserEmailPayload,
+  ) {
+    const { reportedUserEmail, reportedUserUsername, actionTaken } =
+      reportedUserDataDto;
+
+    const caseOfAction =
+      actionTaken.toLowerCase() === 'suspension'
+        ? 'suspended'
+        : actionTaken.toLowerCase() === 'warning'
+          ? 'warned'
+          : 'banned';
+
+    const apologySubject = 'Apology for the Inconvenience';
+    const apologyMessage = `
+<p>Dear ${reportedUserUsername},</p>
+
+<p>
+  We sincerely apologize for any inconvenience caused by the recent moderation action.
+  One of our moderators accidentally ${caseOfAction} you for violating our community guidelines.
+  After reviewing the situation, we have taken the appropriate steps to rectify the issue.
+</p>
+
+<p>
+  If you have any questions or concerns, please feel free to reach out to us through our
+  <a href="${this.configService.get<string>('FRONTEND_URL')}/contact">Contact Us</a> page.
+</p>
+
+<p>
+  Best regards,<br>
+  ${currUser.firstName} ${currUser.lastName}<br>
+  TaleWeaver Moderation Team
+</p>
+
+<p>
+  <i>
+    Please note that responses to this email will not receive a response.
+    If you have any questions or concerns, please use the Contact Us page linked above.
+  </i>
+</p>
+`;
+
+    await this.resend.emails.send({
+      from: `TaleWeaver <${this.configService.get<string>('RESEND_SENDER_EMAIL')}>`,
+      to: reportedUserEmail,
+      subject: apologySubject,
+      html: apologyMessage,
     });
   }
 }
