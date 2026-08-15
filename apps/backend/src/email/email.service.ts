8c7d98c06149d5bd6576993ce85f87c23b004b8f
Hairum Qureshi <hqureshi@udel.edu>
2026-08-14 20:03:26 -0400

    removed unneeded user data param

--- apps/backend/src/email/email.service.ts
+++ apps/backend/src/email/email.service.ts
@@ -125,7 +125,6 @@ export class EmailService {
   }
 
   async sendModeratorNoticeEmail(
-    currUser: UserPayload,
     reportedUserDataDto: ReportedUserEmailPayload,
   ) {
     const { reportedUserEmail, reportID, emailSubject, emailBody } =
