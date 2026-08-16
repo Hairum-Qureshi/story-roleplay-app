517c0865d0a4d5f211f47aa71d390494d126ee83
Hairum Qureshi <hqureshi@udel.edu>
2026-08-16 19:53:17 -0400

    updated imports

--- apps/frontend/src/components/main.tsx
+++ apps/frontend/src/components/main.tsx
@@ -1,9 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./css/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "../css/index.css";
 
 const queryClient = new QueryClient();
 
