083aa156f4a273a33975e8ebca110c04c6d1893c
Hairum Qureshi <hqureshi@udel.edu>
2026-08-27 23:13:08 -0400

    added update call to update the report model in the suspendUser method

--- apps/backend/src/moderation/moderation.service.ts
+++ apps/backend/src/moderation/moderation.service.ts
@@ -1,10 +1,20 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { UserDocument } from 'src/schemas/User';
+import { Model } from 'mongoose';
+import { ReportDocument } from 'src/schemas/Report';
+import { EmailService } from 'src/email/email.service';
+import { User, UserDocument } from 'src/schemas/User';
 import { ModerationStatus } from 'src/enums/moderation.enum';
+import { UserPayload } from 'src/types';
+import Role from 'src/enums/roles.enum';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class ModerationService {
-  constructor() {}
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<UserDocument>,
+    @InjectModel('Report') private readonly reportModel: Model<ReportDocument>,
+    private readonly emailService: EmailService,
+  ) {}
 
   async checkUserModerationStatus(user: UserDocument) {
     if (user.moderation.status === ModerationStatus.BANNED) {
@@ -26,5 +36,40 @@ export class ModerationService {
         await user.save();
       }
     }
+  }
+
+  async suspendUser(
+    duration: string,
+    userRole: Role,
+    reportID: string,
+    suspendedUserID: string,
+    currUser: UserPayload,
+  ) {
+    if (currUser._id === suspendedUserID)
+      throw new HttpException('You cannot suspend yourself', 403);
+
+    if (userRole == Role.ADMIN && currUser.role !== Role.ADMIN)
+      throw new HttpException(
+        'You cannot suspend an admin. Only another admin can do this',
+        403,
+      );
+
+    if (!duration.trim())
+      throw new HttpException('Duration cannot be empty', 400);
+
+    const suspendedUser: UserDocument | null =
+      await this.userModel.findById(suspendedUserID);
+    if (!suspendedUser) throw new HttpException('User not found', 404);
+
+    await this.userModel.findByIdAndUpdate(suspendedUserID, {
+      'moderation.status': ModerationStatus.SUSPENDED,
+      'moderation.expiresAt': new Date(Date.now() + parseInt(duration) * 1000),
+    });
+
+    await this.reportModel.findByIdAndUpdate(reportID, {
+      moderationStatusTaken: ModerationStatus.SUSPENDED,
+      sentNoticeEmail: true,
+      handledBy: currUser._id,
+    });
   }
 }
